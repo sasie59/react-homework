d25dae32b28f499563789c2f908f75550cdf2ac4
sasie59 <sasie59@gmail.com>
2019-11-12 01:11:56 +0800

    HW7 OK

--- src/PersonTable.js
+++ src/PersonTable.js
@@ -5,14 +5,32 @@ export default class PersonTable extends React.Component {
   constructor(){
     super()
     this.state = ({
-
+      personList :[]
     });
   }
+
+  componentDidMount() {
+    this.handleFetch();
+  }
+  
+
+  handleFetch = () => {
+    fetch('https://randomuser.me/api/?results=25')
+    .then(function(res) {
+      return res.json()
+    })
+    .then((json) => {
+      this.setState({
+        personList: json.results
+      });
+    });
+  }
+
   render() {
     return (
       <div>
         <h2>使用者表格</h2>
-        <button onClick>Update List</button>
+        <button onClick={this.handleFetch}>Update List</button>
         <table>
           <thead>
             <tr>
@@ -26,31 +44,18 @@ export default class PersonTable extends React.Component {
             </tr>
           </thead>
           <tbody>
-            <PersonalInfo />
-            <PersonalInfo />
-            <PersonalInfo />
-            <PersonalInfo />
-            <PersonalInfo />
-            <PersonalInfo />
-            <PersonalInfo />
-            <PersonalInfo />
-            <PersonalInfo />
-            <PersonalInfo />
-            <PersonalInfo />
-            <PersonalInfo />
-            <PersonalInfo />
-            <PersonalInfo />
-            <PersonalInfo />
-            <PersonalInfo />
-            <PersonalInfo />
-            <PersonalInfo />
-            <PersonalInfo />
-            <PersonalInfo />
-            <PersonalInfo />
-            <PersonalInfo />
-            <PersonalInfo />
-            <PersonalInfo />
-            <PersonalInfo />
+            {this.state.personList.map(person =>{
+              return <PersonalInfo
+                key={person.login.uuid}
+                picture={person.picture.thumbnail}
+                name={person.name.last}
+                gender={person.gender}
+                email={person.email}
+                age={person.dob.age}
+                phone={person.phone}
+                country={person.location.country}
+              />
+            })}
           </tbody>
         </table>
       </div>
--- src/PersonalInfo.js
+++ src/PersonalInfo.js
@@ -3,13 +3,14 @@ import React from 'react'
 export default function PersonalInfo(props) {
   return (
     <tr>
-      <td>{props.picture}</td>
+    
+      <td><img src={props.picture} alt=""/></td>
       <td>{props.name}</td>
       <td>{props.gender}</td>
       <td>{props.email}</td>
       <td>{props.age}</td>
       <td>{props.phone}</td>
-      <td>{props.conutey}</td>
+      <td>{props.country}</td>
     </tr>
   )
 }
