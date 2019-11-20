fe7f4864165139e64fa63bb11fa21f0aef9b4843
sasie59 <sasie59@gmail.com>
2019-11-20 23:21:17 +0800

    HW12  OK

--- src/Tab.js
+++ src/Tab.js
@@ -4,39 +4,45 @@ export default class Tab extends Component {
   constructor() {
     super()
     this.state = {
-      value :'',
-      content1 : "我是內容1",
-      content2 : "我是內容2",
-      content3 : "我是內容3",
+      arr :['內容1', '內容2', '內容3'],
+      index : 0
     }
   }
-  handleClick = ({target}) => {
+  handleClick = (index) => {
     this.setState ({
-      value : target.value,
+      index
     });
   }
   
   render() {
-    const {value,content1,content2,content3} = this.state;
-    // if(value === '') {
-    //   let value = content1
-    // }
-    // 如何指定按鈕1一開始就被選定?
+    const {arr,index} = this.state;
     return (
       <div>
-        <button
-          name={1}
-          value={content1}
+      {['tab1', 'tab2', 'tab3'].map((tabName,index) =>
+        <span
+        key={index}
+        style={{
+          color : 'blue',
+          padding : '3px',
+          border : '1px solid black',
+          backgroundColor : 'lightgray'
+        }}
+        // 要習慣css在這裡的寫法 屬性的寫法:小駝峰式 內容要單引號包住
+        onClick={this.handleClick.bind(this, index)
+        }>
+          {tabName}
+        </span>
+      )}
+        {/* <button
+          name={0}
           onClick={this.handleClick}>Tab1</button>
         <button
-          name={2}
-          value={content2}
+          name={1}
           onClick={this.handleClick}>Tab2</button>
         <button
-          name={3}
-          value={content3}
-          onClick={this.handleClick}>Tab3</button><br />
-        {value}
+          name={2}
+          onClick={this.handleClick}>Tab3</button><br /> */}
+        <br />{arr[index]}
       </div>
     )
   }
