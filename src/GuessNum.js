6e7134c448f314fe04e4f8a41f9e50a2f4535547
sasie59 <sasie59@gmail.com>
2019-12-25 01:14:42 +0800

    HW20 再讓我想想 我可以的

--- src/GuessNum.js
+++ src/GuessNum.js
@@ -6,17 +6,32 @@ export default class GuessNum extends Component {
     this.state = {
       value : '',
       minValue: 1,
-      maxValue: 1000
+      maxValue: 1000,
+      bingoNum : Math.floor(Math.random() * 1001) + 1
     };
   }
   handleChange = ({target}) => {
     this.setState({
       value : target.value
     })
+    console.warn(target.value);
   }
 
   handleSubmit = (event) => {
     event.preventDefault()
+    const{minValue,maxValue,value} = this.state;
+    const newMin = Math.abs(value - minValue);
+    const newMax = Math.abs(value - maxValue);
+    const min = newMin < newMax ? value : minValue;
+    const max = newMax < newMin ? value : maxValue;
+    this.setState({
+      minValue : min,
+      maxValue : max,
+      value : ''
+    })
+    console.warn(value);
+    console.warn(newMin);
+    
   }
 
   render() {
@@ -26,7 +41,7 @@ export default class GuessNum extends Component {
           現在範圍 :{this.state.minValue} ~ {this.state.maxValue}
         </h1>
         <form onSubmit={this.handleSubmit}>
-          <input type="text" value={this.state.value}onChange={this.handleChange}/>
+          <input type="text" value={this.state.value} onChange={this.handleChange}/>
           <button>submit</button>
         </form>
       </div>
