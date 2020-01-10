cbfcc7498a7fe6d21ed9db684ff0efa63571d1ff
sasie59 <sasie59@gmail.com>
2020-01-10 11:20:27 +0800

    hw23 切換早上晚上的按扭

--- src/SunMoon.js
+++ src/SunMoon.js
@@ -5,14 +5,25 @@ export default class SunMoon extends Component {
   constructor (){
     super ();
     this.state = {
-
+      evening : '晚上',
+      morning : '早上',
+      hideAndSee : true,
     }
   }
+
+  handleClick = () => {
+    const change = this.state.hideAndSee ? false : true
+    this.setState({
+      hideAndSee : change
+    })
+  }
+
   render() {
+    const {evening, morning, hideAndSee} = this.state;
     return (
       <div>
         <div className='center'>請仔細觀察太陽與月亮的位置，「必須」對準正中央
-          <button onClick={this.handleClick}>切換成 晚上</button>
+          <button onClick={this.handleClick}>切換成 {hideAndSee ? `${evening}` : `${morning}`}</button>
         </div>
         <img src="https://img.icons8.com/cotton/256/000000/sun--v1.png" alt=" sum"/>
         <img src="https://img.icons8.com/cotton/256/000000/moon-satellite.png" alt="moon"/>
