fbcff678758a5ebfe58cdc14b7e32ffad7cae625
sasie59 <sasie59@gmail.com>
2020-01-11 01:09:28 +0800

    hw23 有些問題 中午會公出 回來投票完 再回去上班 先睡了 星期六的班是場硬戰

--- src/SunMoon.js
+++ src/SunMoon.js
@@ -5,28 +5,38 @@ export default class SunMoon extends Component {
   constructor (){
     super ();
     this.state = {
-      evening : '晚上',
-      morning : '早上',
       hideAndSee : true,
     }
   }
 
+  /**
+   * 這題有很多做法，我的做法是，先定義一個 div 上面是太陽，下面是月亮
+   * 然後藉由開關來移動整個 div
+   *  ---------- ← div
+   * |   SUN   | ← 裡面有 img 太陽
+   * |         |
+   * |   MON   | ← 裡面是 img 月亮
+   * ----------
+   * Q1 為何背景只有區塊黑
+   * Q2 想在img裡再多套用一個css來調位置 但已有true false的關係 
+   *    可以直接加在後面嗎
+   * 
+   */
   handleClick = () => {
-    const change = this.state.hideAndSee ? false : true
     this.setState({
-      hideAndSee : change
+      hideAndSee : !this.state.hideAndSee
     })
   }
 
   render() {
-    const {evening, morning, hideAndSee} = this.state;
+    const {hideAndSee} = this.state;
     return (
-      <div>
+      <div className={hideAndSee ? 'morn' : 'night'}>
         <div className='center'>請仔細觀察太陽與月亮的位置，「必須」對準正中央
-          <button onClick={this.handleClick}>切換成 {hideAndSee ? `${evening}` : `${morning}`}</button>
+          <button onClick={this.handleClick}>切換成 {hideAndSee ? '晚上' : '早上'}</button>
+          <img className={hideAndSee ? 'moveDown' : 'moveUp'}src="https://img.icons8.com/cotton/256/000000/sun--v1.png" alt="sum"/>
+          <img className={hideAndSee ? 'moveUp' : 'moveDown'} src="https://img.icons8.com/cotton/256/000000/moon-satellite.png" alt="moon"/>
         </div>
-        <img src="https://img.icons8.com/cotton/256/000000/sun--v1.png" alt=" sum"/>
-        <img src="https://img.icons8.com/cotton/256/000000/moon-satellite.png" alt="moon"/>
       </div>
     )
   }
