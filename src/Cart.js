fcdc65b997a43ed57a6d3f5df336ac6872245b28
sasie59 <sasie59@gmail.com>
2020-01-17 02:47:31 +0800

    HW22  再給我點時間 我一定做的出來

--- src/Cart.js
+++ src/Cart.js
@@ -14,17 +14,52 @@ export default class Cart extends Component {
   constructor(){
     super()
     this.state = {
-      value : ''
+      value : '',
+      isJoin : false,
+      sum : 0,
     }
   }
-  render() {
-    console.warn(fruitList[0]);
 
+  handleSubmit = (event) => {
+    const money = fruitList.price * this.state.count
+    event.preventdefault()
+    this.setState ({
+      value : '',
+      isJoin : true,
+      [event.target.count] : +event.target.value + this.state.count,
+      sum : this.state.sum + money
+    })
+  }
+
+  handleChange = ({target}) => {
+    this.setState ({
+      [target.name] : +target.value,
+    })
+  }
+  
+  render() {
+    const {count} = this.state;
+    const money = fruitList.price * this.state.count
+    const itemList = fruitList.map(fruit =>(
+      <li key={fruit.name}> {fruit.name} * {count} = {money}</li>
+      // 我這邊的count有問題 so出現nan
+    ))
     return (
       <div>
         {fruitList.map(fruit => (
-        <CartFruit  key={fruit.name} name={fruit.name} price={fruit.price}/>
+        <CartFruit
+          count={this.state.count}
+          onSubmit={this.handleSubmit}
+          onChange={this.handleChange}
+          key={fruit.name}
+          name={fruit.name}
+          price={fruit.price}/>
         ))}
+        <ul>
+          {itemList}
+          <li>========</li>
+          <li>${this.state.sum}</li>
+        </ul>
       </div>
     )
   }
--- src/CartFruit.js
+++ src/CartFruit.js
@@ -6,7 +6,7 @@ export default function CartFruit(props) {
       <span>{props.name}</span>
       <span>${props.price}</span>
       <form onSubmit={props.onSubmit}>
-        <input type="number" placeholder='數量'/>
+        <input onChange={props.onChange} type="number" placeholder='數量'/>
         <button>加入購物車</button>
       </form>
     </div>
