17217841ad5581a5d13fd3a7884866774b028156
sasie59 <sasie59@gmail.com>
2020-01-21 03:15:05 +0800

    hw22 ok css今晚下班再補上

--- src/Cart.js
+++ src/Cart.js
@@ -2,68 +2,100 @@ import React, { Component } from 'react';
 import './Cart.css';
 import CartFruit from './CartFruit.js'
 
-const fruitList = [
-  {name: '橘子', price: 20},
-  {name: '蓮霧', price: 30},
-  {name: '哈蜜瓜', price: 40},
-  {name: '蘋果', price: 50},
-  {name: '葡萄柚', price: 15},
-]
+const fruitObj = {
+  orange:     {name: '橘子',   price: 20},
+  waxApple:   {name: '蓮霧',   price: 30},
+  cantaloupe: {name: '哈蜜瓜', price: 40},
+  apple:      {name: '蘋果',   price: 50},
+  grapeFruit: {name: '葡萄柚', price: 15},
+}
 
 export default class Cart extends Component {
   constructor(){
     super()
     this.state = {
       value : '',
-      isJoin : false,
       sum : 0,
-      count : 0
+      fruitCount: {
+        orange: 0,
+        waxApple: 0,
+        cantaloupe: 0,
+        apple: 0,
+        grapeFruit: 0
+      }
     }
   }
 
-  handleSubmit = (event) => {
-    event.preventdefault()
-    const money = fruitList.price * this.state.count
+  handleSubmit = (fruitName, value) => {
+    const money = fruitObj[fruitName].price * value;
+    const { fruitCount } = this.state;
+    fruitCount[fruitName] += value;
     this.setState ({
       value : '',
-      isJoin : true,
       sum : this.state.sum + money,
-      [event.target.count] : event.target.value + this.state.count,
-      // count : this.state.count + event.target.value
-      // 我覺得我count的寫法怪怪的
-    })
-    console.warn(this.state.count);
-    // 畫面一閃而過 出現錯誤 但也印不count
+      fruitCount: fruitCount,
+    });
   }
 
-  handleChange = ({target}) => {
-    this.setState ({
-      [target.name] : target.value,
+  handleLess = (fruitName) => {
+    const { fruitCount, sum} = this.state;
+    fruitCount[fruitName] -= 1;
+    this.setState({
+      fruitCount :  fruitCount,
+      sum : sum - fruitObj[fruitName].price * 1
     })
-    console.warn([target.name], target.value);
-    // [target.name]印不出東西來
+  }
+
+  handleClear = (fruitName) => {
+    const {fruitCount, sum} = this.state;
+    this.setState({
+      sum : sum - fruitObj[fruitName].price * fruitCount[fruitName]
+    })
+    fruitCount[fruitName] = 0
+    this.setState({
+      fruitCount : fruitCount
+    })
   }
   
   render() {
-    const {count} = this.state;
-    const itemList = fruitList.map(fruit =>(
-      <li key={fruit.name}> {fruit.name} * {count} = {fruit.price * count}</li>
-    ))
+    const {fruitCount} = this.state;
+    const itemList = Object.keys(fruitObj)
+      .filter(fruitName => {
+        return fruitCount[fruitName] > 0;
+      })
+      .map(fruitName =>(
+        <li key={fruitObj[fruitName].name}>
+          {fruitObj[fruitName].name} * 
+          {fruitCount[fruitName]} = 
+          {fruitObj[fruitName].price * fruitCount[fruitName]}
+          <button onClick={this.handleLess.bind(this,fruitName)}>-1</button>
+          <button onClick={this.handleClear.bind(this,fruitName)}>clear</button>
+        </li>
+      ));
     return (
       <div>
-        {fruitList.map(fruit => (
+
+        {Object.keys(fruitObj).map((fruitName) => (
         <CartFruit
-          count={this.state.count}
+          count={this.state.fruitCount}
           onSubmit={this.handleSubmit}
-          onChange={this.handleChange}
-          key={fruit.name}
-          name={fruit.name}
-          price={fruit.price}/>
+          key={fruitObj[fruitName].name}
+          name={fruitObj[fruitName].name}
+          price={fruitObj[fruitName].price}
+          fruitName={fruitName}
+        />
         ))}
+        {/* {true && <MyComp />}
+        {false && <MyComp />}
+        {isShow && <MyComp />} */}
         <ul>
           {itemList}
-          <li>========</li>
-          <li>${this.state.sum}</li>
+          {
+            !!this.state.sum && <>
+              <li>========</li>
+              <li>${this.state.sum}</li>
+            </>
+          }
         </ul>
       </div>
     )
--- src/CartFruit.js
+++ src/CartFruit.js
@@ -1,14 +1,43 @@
 import React from 'react';
 
-export default function CartFruit(props) {
-  return (
-    <div>
-      <span>{props.name}</span>
-      <span>${props.price}</span>
-      <form onSubmit={props.onSubmit}>
-        <input onChange={props.onChange} count={props.count} type="number" placeholder='數量'/>
-        <button>加入購物車</button>
-      </form>
-    </div>
-  )
+export default class CartFruit extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      value: 0,
+    }
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.onSubmit(this.props.fruitName, this.state.value);
+    this.setState({
+      value : ''
+    })
+  }
+
+  handleChange = ({target}) => {
+    this.setState ({
+      value : +target.value
+    });
+  }
+
+  render() {
+    const { name, price } = this.props;
+    return (
+      <div>
+        <span>{name}</span>
+        <span>${price}</span>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="number"
+            onChange={this.handleChange}
+            value={this.state.value}
+            placeholder='數量'/>
+          <button>加入購物車</button>
+        </form>
+      </div>
+    );
+  }
 }
