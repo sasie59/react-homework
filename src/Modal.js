c9a346575c7015a247a7389fc29b804fd061a4d4
sasie59 <sasie59@gmail.com>
2020-01-31 01:26:13 +0800

    新題目 下班再來估時間

---
+++ src/Modal.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export default class Modal extends Component {
+  constructor(){
+    super();
+    this.state = {
+      
+    }
+  }
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
