0dcdf58afb7c279ba646652a998361ff3a930501
sasie59 <sasie59@gmail.com>
2020-03-04 01:19:13 +0800

    HW27 我用我的囉輯去寫 但css效果錯誤

--- src/Carousel.js
+++ src/Carousel.js
@@ -1,17 +1,72 @@
-import React, { Component } from 'react';
-import CarouselCpm from './CarouselCpm';
-import './Carousel.css'
+// import React, { Component } from 'react';
+// import CarouselCpm from './CarouselCpm';
+// import './Carousel.css'
 
+// export default class Carousel extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <CarouselCpm showDots time={1500}>
+//           <div className='red'>1</div>
+//           <div className='green'>2</div>
+//           <div className='blue'>3</div>
+//         </CarouselCpm> 
+//       </div>
+//     )
+//   }
+// }
+import React, { Component } from 'react'
+import CarouselCpm from './CarouselCpm'
 export default class Carousel extends Component {
   render() {
     return (
       <div>
-        <CarouselCpm showDots time={1500}>
-          <div className='red'>1</div>
-          <div className='green'>2</div>
-          <div className='blue'>3</div>
-        </CarouselCpm> 
+        <CarouselCpm time={1500}>
+          <div style={{
+            'backgroundColor': 'red'
+          }}>1</div>
+          <div style={{
+            'backgroundColor': 'green'
+          }}>2</div>
+          <div style={{
+            'backgroundColor': 'blue'
+          }}>3</div>
+        </CarouselCpm>
       </div>
     )
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/CarouselCpm.js
+++ src/CarouselCpm.js
@@ -1,44 +1,82 @@
-import React from "react";
-import { Component } from "react";
-import "./Carousel.css";
+// import React from "react";
+// import { Component } from "react";
+// import "./Carousel.css";
 
+// export default class CarouselCpm extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       itemList: props.children
+//     };
+//     console.warn(this.state.itemList);
+//   }
+
+//   componentDidMount() {
+//     setInterval(() => {
+//       this.setState({isMoving: true})
+//       setTimeout(()=> {
+//         const {itemList} = this.state;
+//         this.setState({
+//           itemList: itemList.slice(1).concat(itemList[0])
+//       });
+//       this.setState({isMoving: false});
+//       }, 500)
+//       // console.warn(itemList);
+//     }, 1500);
+//   }
+  
+
+//   render() {
+//     return (
+//       <div className="Carousel">
+//         {this.state.itemList.map((item,index) =>
+//           <div className={'item' +
+//           (index === 0 ? ' active': '') +
+//           (this.state.isMoving && index < 2 ? ' move': '')
+//           }
+//           key={index} >
+//             {item}
+//           </div>
+//         )}
+//       </div>
+//     );
+//   }
+// }
+import React, { Component } from 'react'
+import './Carousel.css'
 export default class CarouselCpm extends Component {
-  constructor(props) {
-    super(props);
+  constructor(props){
+    super(props)
     this.state = {
       itemList: props.children
-    };
-    console.warn(this.state.itemList);
+    }
   }
 
-  componentDidMount() {
+  componentDidMount(){
     setInterval(() => {
-      this.setState({isMoving: true})
-      setTimeout(()=> {
-        const {itemList} = this.state;
+      setTimeout(() => {
+        const { itemList } = this.state;
         this.setState({
           itemList: itemList.slice(1).concat(itemList[0])
-      });
-      this.setState({isMoving: false});
-      }, 500)
-      // console.warn(itemList);
+        });
+      },500);
     }, 1500);
   }
   
-
   render() {
+    const {itemList} = this.state;
     return (
-      <div className="Carousel">
-        {this.state.itemList.map((item,index) =>
-          <div className={'item' +
-          (index === 0 ? ' active': '') +
-          (this.state.isMoving && index < 2 ? ' move': '')
-          }
-          key={index} >
-            {item}
+      <div className='carousel'>
+        <div className='item'>
+          <div className='hide'>
+            {itemList[0]}
           </div>
-        )}
+          <div className='show'>
+            {itemList[1]}
+          </div>
+        </div>
       </div>
-    );
+    )
   }
 }
+
