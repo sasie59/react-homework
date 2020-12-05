696cb0054c5c913b9400e4fd4d5b17387ca3d7d2
sasie59 <33227762+sasie59@users.noreply.github.com>
2020-12-05 14:53:38 +0000

    Deploying to gh-pages from master e9033350ef6387bacf33c7c3a5c804db5cde8364

---
+++ precache-manifest.c555568d4229a8e1eeb1898e1239a0a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d51516995c36fcb786761ea289374c6d",
+    "url": "/react-homework/index.html"
+  },
+  {
+    "revision": "f917d9ec5f8936a945ec",
+    "url": "/react-homework/static/css/main.5d44a621.chunk.css"
+  },
+  {
+    "revision": "2fa0f8aa9c287ac224cf",
+    "url": "/react-homework/static/js/2.905a2eb4.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-homework/static/js/2.905a2eb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f917d9ec5f8936a945ec",
+    "url": "/react-homework/static/js/main.88be38fb.chunk.js"
+  },
+  {
+    "revision": "6a0c3be9455dcc87d0e4",
+    "url": "/react-homework/static/js/runtime-main.0515a2e1.js"
+  }
+]);
