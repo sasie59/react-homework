a5f451c10d29ffab3dfa7d7157d5aa1db2276fbb
sasie59 <sasie59@gmail.com>
2020-12-06 19:56:19 +0800

    改hook寫第一題

--- src/HW1Counter.js
+++ src/HW1Counter.js
@@ -1,23 +1,43 @@
-import React  from 'react';
 
-export default class Counter extends React.Component {
-  constructor(){
-    super();
-    this.state = ({
-      counter: 0 
-    });
-  }
-  handleChangeCounter = () => {
-    this.setState({
-      counter: this.state.counter + 1
-    });
-  }
-  render() {
-    return (
-      <div>
-        <h1>HW1Counter</h1>
-        <button onClick={this.handleChangeCounter}>counter : {this.state.counter}</button>
-      </div>
-    );
-  }
+import React, { useState } from 'react';
+
+export default function Counter() {
+  const [ count, setCount] = useState(0);
+  const [ count2, setCount2] = useState(0);
+
+  const handleClick = () => {
+    setCount2(count2 + 1);
+  };
+
+  return (
+    <div>
+      <h1>HW1Counter</h1>
+      {/* 兩種寫法 */}
+      <button onClick={() => setCount(count + 1)}>按我 : {count}</button><br/>
+      <button onClick={handleClick}>click me : {count2}</button>
+    </div>
+  );
 }
+
+
+// export default class Counter extends React.Component {
+//   constructor(){
+//     super();
+//     this.state = ({
+//       counter: 0 
+//     });
+//   }
+//   handleChangeCounter = () => {
+//     this.setState({
+//       counter: this.state.counter + 1
+//     });
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <h1>HW1Counter</h1>
+//         <button onClick={this.handleChangeCounter}>counter : {this.state.counter}</button>
+//       </div>
+//     );
+//   }
+// }
