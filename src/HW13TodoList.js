9f5bf44cf6dbbfdb3eeca23ac7049565bf861836
sasie59 <sasie59@gmail.com>
2020-12-15 08:15:34 +0800

    hw13 改版面 上班去

--- src/HW13TodoList.js
+++ src/HW13TodoList.js
@@ -1,77 +1,85 @@
-import React, { Component } from 'react';
-import './HW13TodoList.scss';
+import React ,{ useState }from 'react';
 
-export default class HW13TodoList extends Component {
-  constructor(){
-    super();
-    this.state = {
-      toDoList :[],
-      value : ''
-    };
-  }
-  handleChange = ({target}) => {
-    this.setState({
-      value :target.value
-    });
-  }
+export default function HW13TodoList() {
 
-  handlSubmit = (event) => {
-    const {value,toDoList} = this.state;
-    event.preventDefault();
+  const [list, setList ] = useState([]);
+  const [value, setValue ] = useState('');
 
-    if(value.trim() !== '')
-      this.setState({
-        toDoList : [{content :value, done :false}, ...toDoList],
-        value : ''
-      });
-  }
-  /**
-  [
-    {content: 'aaa', done: false },
-    {content: 'bbb', done: false },
-    {content: 'ccc', done: false },
-  ],
-  [
-    {content: 'aaa', done: false },
-    {content: 'bbb', done: true },
-    {content: 'ccc', done: false },
-  ]
-   */
-  handleDelete = (index) => {
-    const { toDoList } = this.state;
-    toDoList[index].done = !toDoList[index].done;
-
-    this.setState({
-      toDoList
-    });
-  }
-  render() {
-    const { value,toDoList } = this.state;
-    const ToDoList = toDoList.map((item,index) =>
-      <tr key={index}>
-        <td className={item.done ? "done" : ""}>
-          <label onClick={this.handleDelete.bind(this, index)}>
-            {item.content}
-          </label>
-        </td>
-      </tr>
-    );
-    return (
-      <div>
-        <form onSubmit={this.handlSubmit}>
-          <input
-            type="text"
-            value={value}
-            placeholder="todo..."
-            onChange={this.handleChange}/>
-          <button>Create!</button>
-        </form>
-        <table>
-          <tbody>
-            {ToDoList}
-          </tbody>
-        </table>  
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h1>HW13TodoList</h1>
+      <h1>代辦事項</h1>
+      <form action="">
+        <input type="text"/>
+      </form>
+    </div>
+  );
 }
+
+
+// import React, { Component } from 'react';
+// import './HW13TodoList.scss';
+
+// export default class HW13TodoList extends Component {
+//   constructor(){
+//     super();
+//     this.state = {
+//       toDoList :[],
+//       value : ''
+//     };
+//   }
+//   handleChange = ({target}) => {
+//     this.setState({
+//       value :target.value
+//     });
+//   }
+
+//   handlSubmit = (event) => {
+//     const {value,toDoList} = this.state;
+//     event.preventDefault();
+
+//     if(value.trim() !== '')
+//       this.setState({
+//         toDoList : [{content :value, done :false}, ...toDoList],
+//         value : ''
+//       });
+//   }
+
+//   handleDelete = (index) => {
+//     const { toDoList } = this.state;
+//     toDoList[index].done = !toDoList[index].done;
+
+//     this.setState({
+//       toDoList
+//     });
+//   }
+//   render() {
+//     const { value,toDoList } = this.state;
+//     const ToDoList = toDoList.map((item,index) =>
+//       <tr key={index}>
+//         <td className={item.done ? "done" : ""}>
+//           <label onClick={this.handleDelete.bind(this, index)}>
+//             {item.content}
+//           </label>
+//         </td>
+//       </tr>
+//     );
+//     return (
+//       <div>
+//         <form onSubmit={this.handlSubmit}>
+//           <input
+//             type="text"
+//             value={value}
+//             placeholder="todo..."
+//             onChange={this.handleChange}/>
+//           <button>Create!</button>
+//         </form>
+//         <table>
+//           <tbody>
+//             {ToDoList}
+//           </tbody>
+//         </table>  
+//       </div>
+//     );
+//   }
+// }
