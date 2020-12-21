2486720869101c549ff6b344e48291d1e2047228
寫詩羊 <Habuche@gmail.com>
2020-12-21 21:35:54 +0800

    fix bug

--- src/HW1Counter.js
+++ src/HW1Counter.js
@@ -2,7 +2,7 @@
 import React, { useState } from 'react';
 
 export default function Counter() {
-  const [ count, setCount] = useState(0);
+  // const [ count, setCount] = useState(0);
   const [ count2, setCount2] = useState(0);
 
   const handleClick = () => {
