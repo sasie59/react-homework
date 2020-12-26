860bd87f11576a70bc5cac8c66496179ea2ac307
sasie59 <sasie59@gmail.com>
2020-12-26 20:15:20 +0800

    test HW8 is OK

--- src/HW8SumForButton.js
+++ src/HW8SumForButton.js
@@ -7,13 +7,14 @@ export default function HW8SumForButton() {
   return (
     <div>
       <h1>HW8SumForButton</h1>
-      {[1,2,3,4,5,6,7,8,9].map(value => 
+      {[1,2,3,4,5,6,7,8,9].map((value,index) => 
         <button
+          data-testid={index}
           key={value}
           onClick={() => setSum(sum + value)}
         >{value}</button>
       )}
-      <div>加總值 : {sum}</div>
+      <div name='sum'>加總值 : {sum}</div>
     </div>
   );
 }
---
+++ src/HW8SumForButton.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {getByTestId, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import HW from './HW8SumForButton';
+
+test('should HW6RemovableList', () => {
+  const { container } = render(<HW />);
+  expect(container).toMatchSnapshot();
+});
+
+test('should sum = sum + number', () => {
+  const { container, getByTestId } = render(<HW />);
+  const div = container.querySelector('div[name="sum"]');
+  userEvent.click(getByTestId(0));
+  userEvent.click(getByTestId(2));
+
+  expect(div.textContent).toBe('加總值 : 4');
+  expect(container).toMatchSnapshot();
+});
+test('should sum = sum + number', () => {
+  const { container, getByTestId } = render(<HW />);
+  const div = container.querySelector('div[name="sum"]');
+  userEvent.click(getByTestId(8));
+  userEvent.click(getByTestId(6));
+
+  expect(div.textContent).toBe('加總值 : 16');
+  expect(container).toMatchSnapshot();
+});
