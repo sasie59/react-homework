e7266b8def1a7169f74b633bdd0a0f289174dbf1
sasie59 <sasie59@gmail.com>
2021-01-29 07:15:22 +0800

    global.fetch 報錯 下班繼續

--- src/HW14Pokemon.test.js
+++ src/HW14Pokemon.test.js
@@ -1,19 +1,29 @@
 import React from "react";
-import { render, act } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
+import { render } from "@testing-library/react";
+// import userEvent from "@testing-library/user-event";
 import HW from './HW14Pokemon';
 
-test('should HW14Pokemon', () => {
-  const { container } = render(<HW />);
-  expect (container).toMatchSnapshot();
-});
-
-test('should HW14Pokemon', () => {
-  const { container } = render(<HW />);
-  const input = container.querySelector('input');
-  userEvent.type(input,"ab{enter}");
-  expect(container).toMatchSnapshot();
-
-
-  // expect (container).toMatchSnapshot();
-});
+global.fetch = jest.fn(
+  () => 
+  new Promise((resolve) => {
+    resolve({
+      json: () =>
+        new Promise((resolve) => {
+          resolve({
+            res: [
+              {
+            count :1118,
+            next:"https://pokeapi.co/api/v2/pokemon/?offset=100&limit=3",
+            previous:null,
+            results:[
+              {name:"bulbasaur", url:"https://pokeapi.co/api/v2/pokemon/1/"},
+              {name:"ivysaur",url:"https://pokeapi.co/api/v2/pokemon/2/"},
+              {name:"venusaur",url:"https://pokeapi.co/api/v2/pokemon/3/"},
+          ],
+        }
+      ]
+      })
+    })
+  })
+  }
+  );
