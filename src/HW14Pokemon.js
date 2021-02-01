3eec441cb8bc4030808e9fdbee243b6129591411
sasie59 <sasie59@gmail.com>
2021-02-01 20:16:08 +0800

    hw14 rcc 重新改寫

--- src/HW14Pokemon.js
+++ src/HW14Pokemon.js
@@ -1,97 +1,99 @@
-import React, { useState, useEffect,useRef } from 'react';
-const imgURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+// import React, { useState, useEffect,useRef } from 'react';
+// const imgURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
 
-export default function HW14Pokemon() {
-  const inputDom = useRef();
-  const [list, setList] = useState([]);
-  
+// export default function HW14Pokemon() {
+//   const inputDom = useRef();
+//   const [list, setList] = useState([]);
 
-  const fetchData = () => {
-    fetch("https://pokeapi.co/api/v2/pokemon/?limit=100")
-      .then(res => res.json())
-      .then(({ results: list }) => {
-        setList(list);
-      });
-  };
-
-  useEffect(()=> {
-    fetchData();
-  },[]);
-
-  return (
-    <div>
-      <h2>順利抓取 100 隻寶可夢的名字與圖片</h2>
-      <h2>可以依名字篩選出寶可夢</h2>
-      <span>
-        <b>keyword</b>
-      </span>
-      <input
-        ref={inputDom}
-        type="text"
-        placeholder="name..."
-      />
-      {list.map(item =>
-        <div key={item.name}>
-          <img src={`${imgURL}${item.url.slice(34, -1)}.png`} alt=""/>
-          <span>{item.name}</span>
-        </div>
-      )}
-    </div>
-  );
-}
-
-
-// import React, { Component } from "react";
-// import PokemonInfo from "./HW14PokemonInfo";
-
-// export default class HW14Pokemon extends Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       pokemonList: [],
-//       value: ""
-//     };
-//   }
-
-//   componentDidMount() {
+//   const fetchData = () => {
 //     fetch("https://pokeapi.co/api/v2/pokemon/?limit=100")
 //       .then(res => res.json())
-//       .then(json =>
-//         this.setState({
-//           pokemonList: json.results
-//         })
-//       );
-//   }
-
-//   handleChange = ({ target }) => {
-//     this.setState({
-//       value: target.value
-//     });
+//       .then(({ results: list }) => {
+//         setList(list);
+//       });
 //   };
 
-//   render() {
-//     const { pokemonList, value } = this.state;
-//     let filterList = [];
-//     filterList = pokemonList.filter(
-//       list => list.name.toLowerCase().indexOf(value.toLowerCase()) > -1
-//     );
-//     return (
-//       <div>
-//         <h2>順利抓取 100 隻寶可夢的名字與圖片</h2>
-//         <h2>可以依名字篩選出寶可夢</h2>
-//         <span>
-//           <b>keyword</b>
-//         </span>
-//         <input
-//           type="text"
-//           placeholder="name..."
-//           keyword={value}
-//           onChange={this.handleChange}
-//         />
-//         {filterList.map(poke => (
-//           <PokemonInfo key={poke.name} name={poke.name} url={poke.url} />
-//         ))}
-//       </div>
-//     );
-//   }
+//   useEffect(()=> {
+//     fetchData();
+//   },[]);
+
+//   return (
+//     <div>
+//       <h2>順利抓取 100 隻寶可夢的名字與圖片</h2>
+//       <h2>可以依名字篩選出寶可夢</h2>
+//       <span>
+//         <b>keyword</b>
+//       </span>
+//       <input
+//         type="text"
+//         ref={inputDom}
+//         placeholder="name..."
+//       />
+//       {list.map(item =>
+//         <div key={item.name}>
+//           <img src={`${imgURL}${item.url.slice(34, -1)}.png`} alt=""/>
+//           <span>{item.name}</span>
+//         </div>
+//       )}
+//     </div>
+//   );
 // }
+
+import React, { Component } from "react";
+const imgURL =
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+
+export default class HW14Pokemon extends Component {
+  constructor() {
+    super();
+    this.state = {
+      pokemonList: [],
+      value: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://pokeapi.co/api/v2/pokemon/?limit=100")
+      .then((res) => res.json())
+      .then((json) =>
+        this.setState({
+          pokemonList: json.results,
+        })
+      );
+  }
+
+  handleChange = ({ target }) => {
+    this.setState({
+      value: target.value,
+    });
+  };
+
+  render() {
+    const { pokemonList, value } = this.state;
+    let filterList = [];
+    filterList = pokemonList.filter(
+      (list) => list.name.toLowerCase().indexOf(value.toLowerCase()) > -1
+    );
+    return (
+      <div>
+        <h2>順利抓取 100 隻寶可夢的名字與圖片</h2>
+        <h2>可以依名字篩選出寶可夢</h2>
+        <span>
+          <b>keyword</b>
+        </span>
+        <input
+          type="text"
+          placeholder="name..."
+          keyword={value}
+          onChange={this.handleChange}
+        />
+        {filterList.map((item) => (
+          <div key={item.name}>
+            <img src={`${imgURL}${item.url.slice(34, -1)}.png`} alt="" />
+            <span>{item.name}</span>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
