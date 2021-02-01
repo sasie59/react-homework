739dcd8859d0bfb363673cb21c9b4e63725aa694
sasie59 <sasie59@gmail.com>
2021-02-01 12:13:48 +0800

    use hooks change wirte HW14(還差filter)

--- src/HW14Pokemon.test.js
+++ src/HW14Pokemon.test.js
@@ -7,30 +7,41 @@ global.fetch = jest.fn(
   () =>
     new Promise((resolve) => {
       resolve({
-        json: () => new Promise((resolve) => {
-          resolve({
-            res: [
-              {
-                count: 1118,
-                next: "https://pokeapi.co/api/v2/pokemon/?offset=100&limit=3",
-                previous: null,
-                results: [
-                  { name: "bulbasaur", url: "https://pokeapi.co/api/v2/pokemon/1/" },
-                  { name: "ivysaur", url: "https://pokeapi.co/api/v2/pokemon/2/" },
-                  { name: "venusaur", url: "https://pokeapi.co/api/v2/pokemon/3/" },
-                ],
-              },
-            ],
-          });
-        }),
+        json: () =>
+          new Promise((resolve) => {
+            resolve({
+              res: [
+                {
+                  count: 1118,
+                  next: "https://pokeapi.co/api/v2/pokemon/?offset=100&limit=3",
+                  previous: null,
+                  results: [
+                    {
+                      name: "bulbasaur",
+                      url: "https://pokeapi.co/api/v2/pokemon/1/",
+                    },
+                    {
+                      name: "ivysaur",
+                      url: "https://pokeapi.co/api/v2/pokemon/2/",
+                    },
+                    {
+                      name: "venusaur",
+                      url: "https://pokeapi.co/api/v2/pokemon/3/",
+                    },
+                  ],
+                },
+              ],
+            });
+          }),
       });
     })
 );
 
-/**  */
-test("should HW14Pokemon", () => {
-  expect(true).toBeTruthy();
+test("should HW14Pokemon", async () => {
+  const { container } = render(<HW />);
+  await waitFor(() => expect(container).toMatchSnapshot());
 });
+
 
 /** 根據我GOOGLE，我發現只要有 fetch 的測試，都要使用 await / async 去接測試，像這樣 */
 // test("should HW14Pokemon", async () => {
