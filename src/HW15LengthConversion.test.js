a6c00a1329e6ab991f9e8b1a374c80c77db4fc46
sasie59 <sasie59@gmail.com>
2021-02-05 07:48:07 +0800

    test hw15 is OK

--- src/HW15LengthConversion.test.js
+++ src/HW15LengthConversion.test.js
@@ -1,20 +1,42 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render,getByTestId } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import HW from './HW15LengthConversion';
 
 test('should input number to lengthType', () => {
-  const { container,getByTestId } = render(<HW />);
-  const input1 = getByTestId('1');
-  const input2 = getByTestId('2');
-  const input3 = getByTestId('3');
-  const input4 = getByTestId('4');
+  const { container } = render(<HW />);
+  const input = container.querySelector('input[name="mm"]');
+  userEvent.type(input, '1000');
+  expect(container.querySelector('input[name="cm"]').value).toBe('100');
+  expect(container.querySelector('input[name="m"]').value).toBe('1');
+  expect(container.querySelector('input[name="km"]').value).toBe('0.001');
+});
 
-  userEvent.type(input1, '100');
-  expect(input2.textContent).toBe('10');
-  expect(input3.textContent).toBe('0.1');
-  expect(input4.textContent).toBe('0.0001');
-  expect(container).toMatchSnapshot();
+test('should input number to lengthType', () => {
+  const { container } = render(<HW />);
+  const input = container.querySelector('input[name="cm"]');
+  userEvent.type(input, '25');
+  expect(container.querySelector('input[name="mm"]').value).toBe('250');
+  expect(container.querySelector('input[name="m"]').value).toBe('0.25');
+  expect(container.querySelector('input[name="km"]').value).toBe('0.00025');
+});
+
+test('should input number to lengthType', () => {
+  const { container } = render(<HW />);
+  const input = container.querySelector('input[name="m"]');
+  userEvent.type(input, '3');
+  expect(container.querySelector('input[name="mm"]').value).toBe('3000');
+  expect(container.querySelector('input[name="cm"]').value).toBe('300');
+  expect(container.querySelector('input[name="km"]').value).toBe('0.003');
+});
+
+test('should input number to lengthType', () => {
+  const { container } = render(<HW />);
+  const input = container.querySelector('input[name="km"]');
+  userEvent.type(input, '1.6');
+  expect(container.querySelector('input[name="mm"]').value).toBe('1600000');
+  expect(container.querySelector('input[name="cm"]').value).toBe('160000');
+  expect(container.querySelector('input[name="m"]').value).toBe('1600');
 });
 
 
