c3cc1c99fd882248c3a0ab5c443f979855456243
sasie59 <sasie59@gmail.com>
2021-02-09 08:10:08 +0800

    改寫中 ~ 上班+面試+回診

--- src/HW16NumberGame.js
+++ src/HW16NumberGame.js
@@ -1,57 +1,78 @@
-import React, { Component } from 'react';
+import React, {useState} from 'react';
 
-const getRandomNumber = () => {
+const randomNumber = () => {
   return Math.floor(Math.random()*10);
-}
+};
+let random = randomNumber();
 
-export default class HW16NumberGame extends Component {
-  constructor(){
-    super();
-    this.state = {
-      random : getRandomNumber(),
-      bingoCount : 0,
-      failCount :0,
-      value :''
-    }
-  }
-  handleChange = ({target}) => {
-    const { random } = this.state;
+
+export default function HW16NumberGame() {
+  const [bingoCount, setBingoCount]  = useState(0);
+  const [failCount, setFailCount]  = useState(0);
+  
+  const handleChange = ({target}) => {
     const result = +target.value === random ? 'bingoCount': 'failCount';
-
-    this.setState({
-      random : getRandomNumber(),
-      [result]: this.state[result] + 1
-      // this.state.bingoCount or this.state.failCont
-    });
-
-    // if(+target.value === random) {
-    //   this.setState({
-    //     random : getRandomNumber(),
-    //     bingoCount : bingoCount + 1
-    //   });
-    // }
-    // else {
-    //   this.setState({
-    //     random : getRandomNumber(),
-    //     failCount : failCount + 1
-    //   });
-    // }
-  }
-
-
-  render() {
-    const {random,bingoCount,failCount,value} = this.state;
-    return (
-      <h1>
-        {random}<br />
-        <input
-          type="text"
-          value={value}
-          onChange={this.handleChange}
-        />
-        <div>答對: {bingoCount}</div>
-        <div>答錯: {failCount}</div>
-      </h1>
-    )
-  }
+  };
+  return (
+    <div>
+      <div>
+        隨機數字
+        {randomNumber()}
+      </div>
+      <input
+        type="text"
+        value=''
+        onChange={handleChange}
+      />
+      <div>答對: {bingoCount}</div>    
+      <div>答錯: {failCount}</div>
+    </div>
+  );
 }
+
+
+
+// import React, { Component } from 'react';
+
+// const getRandomNumber = () => {
+//   return Math.floor(Math.random()*10);
+// };
+
+// export default class HW16NumberGame extends Component {
+//   constructor(){
+//     super();
+//     this.state = {
+//       random : getRandomNumber(),
+//       bingoCount : 0,
+//       failCount :0,
+//       value :''
+//     };
+//   }
+//   handleChange = ({target}) => {
+//     const { random } = this.state;
+//     const result = +target.value === random ? 'bingoCount': 'failCount';
+
+//     this.setState({
+//       random : getRandomNumber(),
+//       [result]: this.state[result] + 1
+//       // this.state.bingoCount or this.state.failCont
+//     });
+//   }
+
+
+//   render() {
+//     const {random,bingoCount,failCount,value} = this.state;
+//     return (
+//       <h1>
+//         {random}<br />
+//         <input
+//           type="text"
+//           value={value}
+//           onChange={this.handleChange}
+//         />
+//         <div>答對: {bingoCount}</div>
+//         <div>答錯: {failCount}</div>
+//       </h1>
+//     );
+//   }
+// }
