72cdbb28b84801989450f25555f0d305e0f002bb
sasie59 <sasie59@gmail.com>
2021-03-03 07:08:35 +0800

    create HW17 test.js

---
+++ src/HW17RandomNum.test.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { render,act } from "@testing-library/react";
+import HW, {numChange } from './HW17RandomNum';
+
+test('should ', () => {
+  act(() =>{
+    
+  });
+});
