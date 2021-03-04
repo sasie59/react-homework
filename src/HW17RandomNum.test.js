c98d1e7f357ff421a5701196239bf510e5579762
sasie59 <sasie59@gmail.com>
2021-03-04 09:16:59 +0800

    test HW17 is OK~~~上班去

--- src/HW17RandomNum.test.js
+++ src/HW17RandomNum.test.js
@@ -1,9 +1,23 @@
 import React from "react";
-import { render,act } from "@testing-library/react";
-import HW, {numChange } from './HW17RandomNum';
+import { render,act, cleanup } from "@testing-library/react";
+import HW, {num, numChange } from './HW17RandomNum';
 
 test('should ', () => {
-  act(() =>{
-    
+  act(() => {
+    jest.useFakeTimers();
+    const { getByTestId } = render(<HW />);
+    const number = getByTestId('num');
+    expect(number.innerText).toBe(num);
+  });
+});
+test('should ', () => {
+  act(() => {
+    jest.useFakeTimers();
+    const { getByTestId } = render(<HW />);
+    let num1 = null;
+    jest.advanceTimersByTime(900);
+    const number = getByTestId('num');
+    num1 = number.innerText;
+    expect(num1).toBe(num);
   });
 });
