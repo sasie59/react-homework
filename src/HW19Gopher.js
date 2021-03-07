655e02e37e63bba6fbe3ee30724742a3fb98477d
sasie59 <sasie59@gmail.com>
2021-03-07 21:15:41 +0800

    use hooks change writ HW19Gopher (吃個飯 等等繼續)

--- src/HW19Gopher.js
+++ src/HW19Gopher.js
@@ -1,35 +1,53 @@
-import React, { Component } from 'react';
-import HW19GopherButton from './HW19GopherButton';
-export default class HW19Gopher extends Component {
-  constructor(){
-    super();
-    this.state = {
-      count : 0,
-    };
-  }
-  
-  changeCount = (result) => {
-    this.setState({
-      count : this.state.count + result
-    });
-  }
-  
-  render() {
-    const {count} = this.state;
-    
-    return (
-      <div>
-        <h1>{count}</h1>
-        <HW19GopherButton changeCount={this.changeCount} />
-        <HW19GopherButton changeCount={this.changeCount} />
-        <HW19GopherButton changeCount={this.changeCount} /><br />
-        <HW19GopherButton changeCount={this.changeCount} />
-        <HW19GopherButton changeCount={this.changeCount} />
-        <HW19GopherButton changeCount={this.changeCount} /><br />
-        <HW19GopherButton changeCount={this.changeCount} />
-        <HW19GopherButton changeCount={this.changeCount} />
-        <HW19GopherButton changeCount={this.changeCount} />
-      </div>
-    );
-  }
+import React, { useState } from "react";
+import GopherButton from "./HW19GopherButton";
+export default function HW19Gopher() {
+  const [count, setCount] = useState(0);
+
+  const changeCount = (result) => {
+    setCount(result + count);
+  };
+  return (
+    <div>
+      <h1>{count}</h1>
+      {[...Array(9).keys()].map((index) => (
+        <GopherButton key={index} changeCount={changeCount} />
+      ))}
+    </div>
+  );
 }
+
+// import React, { Component } from 'react';
+// import HW19GopherButton from './HW19GopherButton';
+// export default class HW19Gopher extends Component {
+//   constructor(){
+//     super();
+//     this.state = {
+//       count : 0,
+//     };
+//   }
+
+//   changeCount = (result) => {
+//     this.setState({
+//       count : this.state.count + result
+//     });
+//   }
+
+//   render() {
+//     const {count} = this.state;
+
+//     return (
+//       <div>
+//         <h1>{count}</h1>
+//         <HW19GopherButton changeCount={this.changeCount} />
+//         <HW19GopherButton changeCount={this.changeCount} />
+//         <HW19GopherButton changeCount={this.changeCount} /><br />
+//         <HW19GopherButton changeCount={this.changeCount} />
+//         <HW19GopherButton changeCount={this.changeCount} />
+//         <HW19GopherButton changeCount={this.changeCount} /><br />
+//         <HW19GopherButton changeCount={this.changeCount} />
+//         <HW19GopherButton changeCount={this.changeCount} />
+//         <HW19GopherButton changeCount={this.changeCount} />
+//       </div>
+//     );
+//   }
+// }
--- src/HW19GopherButton.js
+++ src/HW19GopherButton.js
@@ -1,43 +1,55 @@
-import React, { Component } from 'react';
-// import './HW19Gopher.css';
-import './HW19Gopher.scss';
+import React,{ useState } from 'react';
+import "./HW19Gopher.scss";
 
-export default class HW19GopherButton extends Component {
-  constructor(){
-    super();
-    this.state = {
-      mouse : !Math.floor(Math.random()*2),
-    };
-  }
-  componentDidMount() {
-    this.tick();
-  }
+export default function HW19GopherButton() {
 
-  tick = () => {
-    setTimeout(() => {
-      this.changeNum();
-      this.tick();
-    }, Math.floor(Math.random() * 2000) + 2000);
-  }
-  
-  changeNum = () => {
-    this.setState({
-      mouse : !this.state.mouse
-    });
-  }
-
-  handleClick = () => {
-    this.props.changeCount(this.state.mouse ? 1 : -1);
-    this.setState({
-      mouse : false
-    });
-  }
-
-  render() {
-    return (
-        <button className='GopherButton' onClick={this.handleClick}>
-          <h1>{ this.state.mouse ? 'G': '-' }</h1>
-        </button>
-    );
-  }
+  const [ isG, setisG ] =useState(!Math.floor(Math.random() * 2));
+  return (
+    <div>
+      
+    </div>
+  );
 }
+
+// import React, { Component } from "react";
+// import "./HW19Gopher.scss";
+
+// export default class HW19GopherButton extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       mouse: !Math.floor(Math.random() * 2),
+//     };
+//   }
+//   componentDidMount() {
+//     this.tick();
+//   }
+
+//   tick = () => {
+//     setTimeout(() => {
+//       this.changeNum();
+//       this.tick();
+//     }, Math.floor(Math.random() * 2000) + 2000);
+//   };
+
+//   changeNum = () => {
+//     this.setState({
+//       mouse: !this.state.mouse,
+//     });
+//   };
+
+//   handleClick = () => {
+//     this.props.changeCount(this.state.mouse ? 1 : -1);
+//     this.setState({
+//       mouse: false,
+//     });
+//   };
+
+//   render() {
+//     return (
+//       <button className="GopherButton" onClick={this.handleClick}>
+//         <h1>{this.state.mouse ? "G" : "-"}</h1>
+//       </button>
+//     );
+//   }
+// }
