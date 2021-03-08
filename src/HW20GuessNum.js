e0249331c3769580a9ee6673a37075254a0ae917
sasie59 <sasie59@gmail.com>
2021-03-09 00:00:28 +0800

    use hooks change write HW20(先改寫一些些 讓畫面先出來)

--- src/HW20GuessNum.js
+++ src/HW20GuessNum.js
@@ -1,82 +1,149 @@
-import React, { Component } from "react";
-import './HW20GuessNum.scss';
+import React, { useState } from "react";
 
 const MIN = 1;
 const MAX = 1000;
-
-export const makeRand = (min = MIN, max = MAX) => {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+const makeRand = (min = MIN, max = MAX) => {
+  return ~~(Math.random() * (max - min + 1)) + min;
 };
 
 const resetData = () => {
   return {
     value: "",
-    minValue: MIN,
-    maxValue: MAX,
-    hideAndSee : false,
+    min: MIN,
+    max: MAX,
+    isAppear: false,
     bingoNum: makeRand(MIN, MAX),
   };
 };
 
-export default class HW20GuessNum extends Component {
-  constructor() {
-    super();
-    this.state = resetData();
-  }
+export default function HW20GuessNum() {
+  const [min, setMin] = useState(1);
+  const [max, setMax] = useState(1000);
+  const [value, setValue] = useState("");
+  const [update, setUpdate] = useState({
+    value: "",
+  });
+  const [isAppear, setIsAppear] = useState(false);
+  const [bingoNum, setBingoNum] = useState(makeRand(MIN, MAX));
+  const [reBingoNum, setReBingoNum] = useState();
 
-  handleChange = ({ target: { value } }) => {
-    this.setState({ value });
-  };
-
-  handleSubmit = event => {
-    event.preventDefault();
-    const { bingoNum } = this.state;
-    const value = +this.state.value;
-
-    let updateState = {
-      value: ''
+  const resetData = () => {
+    return {
+      value: "",
+      min: MIN,
+      max: MAX,
+      isAppear: false,
+      bingoNum: makeRand(MIN, MAX),
     };
-
-    if(bingoNum > value) {
-      updateState.minValue = value;
-    } else if (bingoNum < value) {
-      updateState.maxValue = value;
-    } else {
-      updateState.hideAndSee = true;
-    }
-
-    // 看一下 updateState是什麼
-    console.warn(updateState);
-    this.setState(updateState);
+  };
+  const handleSubmit = () => {
   };
 
-  reBingoNum = () => {
-    this.setState(resetData());
-  }
-  // 按下重新一局時 minValue跟maxValue不會變成1跟1000?
+  const handleChange = ({ target }) => {
+    setValue(target.value);
+  };
 
-  render() {
-    return (
-      <div>
-        <h1>
-          現在範圍 :{this.state.minValue} ~ {this.state.maxValue}
-        </h1>
-        <form onSubmit={this.handleSubmit}>
-          <input
-            type="text"
-            value={this.state.value}
-            onChange={this.handleChange}
-            disabled={this.state.hideAndSee}
-          />
-          <button disabled={this.state.hideAndSee}>submit</button>
-        </form>
-        <div className={this.state.hideAndSee ? 'see' : 'hide'}>
-          答對了!答案就是{this.state.bingoNum}
-          <button onClick={this.reBingoNum}>
-            重新一局
-          </button>
-        </div>
+  return (
+    <div>
+      <h1>
+        現在範圍 :{min} ~ {max}
+      </h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={value}
+          onChange={handleChange}
+          disabled={isAppear}
+        />
+        <button disabled={isAppear}>submit</button>
+      </form>
+      <div className={isAppear ? "see" : "hide"}>
+        答對了!答案就是{bingoNum}
+        <button onClick={reBingoNum}>重新一局</button>
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+// import React, { Component } from "react";
+// import './HW20GuessNum.scss';
+
+// const MIN = 1;
+// const MAX = 1000;
+
+// export const makeRand = (min = MIN, max = MAX) => {
+//   return Math.floor(Math.random() * (max - min + 1)) + min;
+// };
+
+// const resetData = () => {
+//   return {
+//     value: "",
+//     minValue: MIN,
+//     maxValue: MAX,
+//     hideAndSee : false,
+//     bingoNum: makeRand(MIN, MAX),
+//   };
+// };
+
+// export default class HW20GuessNum extends Component {
+//   constructor() {
+//     super();
+//     this.state = resetData();
+//   }
+
+//   handleChange = ({ target: { value } }) => {
+//     this.setState({ value });
+//   };
+
+//   handleSubmit = event => {
+//     event.preventDefault();
+//     const { bingoNum } = this.state;
+//     const value = +this.state.value;
+
+//     let updateState = {
+//       value: ''
+//     };
+
+//     if(bingoNum > value) {
+//       updateState.minValue = value;
+//     } else if (bingoNum < value) {
+//       updateState.maxValue = value;
+//     } else {
+//       updateState.hideAndSee = true;
+//     }
+
+//     // 看一下 updateState是什麼
+//     console.warn(updateState);
+//     this.setState(updateState);
+//   };
+
+//   reBingoNum = () => {
+//     this.setState(resetData());
+//   }
+//   // 按下重新一局時 minValue跟maxValue不會變成1跟1000?
+
+//   render() {
+//     return (
+//       <div>
+//         <h1>
+//           現在範圍 :{this.state.minValue} ~ {this.state.maxValue}
+//         </h1>
+//         <form onSubmit={this.handleSubmit}>
+//           <input
+//             type="text"
+//             value={this.state.value}
+//             onChange={this.handleChange}
+//             disabled={this.state.hideAndSee}
+//           />
+//           <button disabled={this.state.hideAndSee}>submit</button>
+//         </form>
+//         <div className={this.state.hideAndSee ? 'see' : 'hide'}>
+//           答對了!答案就是{this.state.bingoNum}
+//           <button onClick={this.reBingoNum}>
+//             重新一局
+//           </button>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
