949f05f3cfe39d0769091fc6af40ff395ece73ad
sasie59 <sasie59@gmail.com>
2021-03-30 00:19:10 +0800

    use hooks change write todoList

--- src/TodoList6/App.js
+++ src/TodoList6/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+// import React from 'react';
+import React,{useState} from 'react';
 import { connect } from 'react-redux';
 
 import { visibilityFilter, ALL, FINISH, UNFINISH } from './actions/visibilityFilter';
@@ -6,67 +7,61 @@ import { addTodo, toggleTodo, removeTodo } from './actions/todos';
 
 import './App.scss';
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      value: ''
-    };
-  }
 
-  handleChange = ({ target }) => {
-    this.setState({ value: target.value });
-  }
+function App(props) {
+  const [value, setValue ] = useState('');
+  const { visibilityFilter: filter } = props;
 
-  handleSubmit = e => {
-    e.preventDefault();
-    this.props.add(this.state.value);
-    this.setState({value: ''});
-  }
-
-  handleToggle = id => {
-    this.props.toggle(id);
-  }
-  handleRemove = id => {
-    this.props.remove(id);
-  }
-
-  handleFilterChange = ({ target }) => {
-    this.props.filter(target.value);
-  }
+  const handleChange = ({target}) => {
+    setValue(target.value);
+  };
   
-  render() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.add(value);
+    setValue('');
+  };
 
-    const { visibilityFilter: filter } = this.props;
+  const handleFilterChange = ({ target }) => {
+    props.filter(target.value);
+  };
 
-    return (
-      <div>
-        <select value={filter} onChange={this.handleFilterChange}>
-          <option value={ALL}>ALL</option>
-          <option value={FINISH}>FINISH</option>
-          <option value={UNFINISH}>UNFINISH</option>
-        </select>
-        <form onSubmit={this.handleSubmit}>
-          <input onChange={this.handleChange} type="text" value={this.state.value} />
-          <button>add</button>
-        </form>
-        {this.props.todos
-          .filter(todo =>
-            filter === FINISH ? todo.isFin:
-              filter === UNFINISH ? !todo.isFin: true
-          )
-          .map(todo =>
-            <div key={todo.id} className={todo.isFin ? 'line': ''}>
-              {todo.text}
-              <button onClick={this.handleToggle.bind(this, todo.id)}>toggle</button>
-              <button onClick={this.handleRemove.bind(this, todo.id)}>remove</button>
-            </div>
-          )
-        }
-      </div>
-    );
-  }
+  const handleToggle = (id) => {
+    props.toggle(id);
+  };
+
+  const handleRemove = (id) => {
+    props.remove(id);
+  };
+  
+  return (
+    <div>
+      <select value={filter} onChange={handleFilterChange}>
+        <option value={ALL}>ALL</option>
+        <option value={FINISH}>FINISH</option>
+        <option value={UNFINISH}>UNFINISH</option>
+      </select>
+      <form onSubmit={handleSubmit}>
+        <input onChange={handleChange} type="text" value={value} />
+        <button>add</button>
+      </form>
+      {props.todos
+        .filter(todo =>
+          filter === FINISH ? todo.isFin:
+            filter === UNFINISH ? !todo.isFin: true
+        )
+        .map(todo =>
+          <div key={todo.id} className={todo.isFin ? 'line': ''}>
+            {todo.text}
+            <button onClick={handleToggle.bind(this, todo.id)}>toggle</button>
+            <button onClick={handleRemove.bind(this, todo.id)}>remove</button>
+          </div>
+        )
+      }
+    </div>
+  );
 }
+
 
 const mapState2Props = state => state;
 const mapDispatch2Props = dispatch => {
@@ -79,3 +74,65 @@ const mapDispatch2Props = dispatch => {
 };
 
 export default connect(mapState2Props, mapDispatch2Props)(App);
+
+// class App extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       value: ''
+//     };
+//   }
+
+//   handleChange = ({ target }) => {
+//     this.setState({ value: target.value });
+//   }
+
+//   handleSubmit = e => {
+//     e.preventDefault();
+//     this.props.add(this.state.value);
+//     this.setState({value: ''});
+//   }
+
+//   handleToggle = id => {
+//     this.props.toggle(id);
+//   }
+//   handleRemove = id => {
+//     this.props.remove(id);
+//   }
+
+//   handleFilterChange = ({ target }) => {
+//     this.props.filter(target.value);
+//   }
+  
+//   render() {
+
+//     const { visibilityFilter: filter } = this.props;
+
+//     return (
+//       <div>
+//         <select value={filter} onChange={this.handleFilterChange}>
+//           <option value={ALL}>ALL</option>
+//           <option value={FINISH}>FINISH</option>
+//           <option value={UNFINISH}>UNFINISH</option>
+//         </select>
+//         <form onSubmit={this.handleSubmit}>
+//           <input onChange={this.handleChange} type="text" value={this.state.value} />
+//           <button>add</button>
+//         </form>
+//         {this.props.todos
+//           .filter(todo =>
+//             filter === FINISH ? todo.isFin:
+//               filter === UNFINISH ? !todo.isFin: true
+//           )
+//           .map(todo =>
+//             <div key={todo.id} className={todo.isFin ? 'line': ''}>
+//               {todo.text}
+//               <button onClick={this.handleToggle.bind(this, todo.id)}>toggle</button>
+//               <button onClick={this.handleRemove.bind(this, todo.id)}>remove</button>
+//             </div>
+//           )
+//         }
+//       </div>
+//     );
+//   }
+// }
