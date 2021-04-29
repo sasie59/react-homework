a4fbd9f582bc3b78c85da31f21db29a9bba20289
sasie59 <sasie59@gmail.com>
2021-04-29 22:24:35 +0800

    hw4 複習React & Hooks & Jest

--- src/HW4Bmi.js
+++ src/HW4Bmi.js
@@ -1,36 +1,36 @@
-import React ,{ useRef }from 'react';
+// import React ,{ useRef }from 'react';
 
-export default function HW4Bmi() {
-  const cmDOM = useRef();
-  const kgDOM = useRef();
-  const bmiDOM = useRef();
+// export default function HW4Bmi() {
+//   const cmDOM = useRef();
+//   const kgDOM = useRef();
+//   const bmiDOM = useRef();
 
-  const handleClick = () => {
-    const cm = cmDOM.current.value;
-    const kg = kgDOM.current.value;
-    bmiDOM.current.innerText = Math.floor(kg / Math.pow(cm / 100, 2));
-  };
+//   const handleClick = () => {
+//     const cm = cmDOM.current.value;
+//     const kg = kgDOM.current.value;
+//     bmiDOM.current.innerText = Math.floor(kg / Math.pow(cm / 100, 2));
+//   };
 
-  return (
-    <div>
-      <h1>HW4Bmi</h1>
-      <span>身高</span>
-      <input
-        ref={cmDOM}
-        name='cm'
-        type="text"
-      /><br/>
-      <span>體重</span>
-      <input
-        ref={kgDOM}
-        name='kg'
-        type="text"
-      /><br/>
-      <button onClick={handleClick}>計算</button>
-      BMI : <span data-testid="bmi" ref={bmiDOM} />
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <h1>HW4Bmi</h1>
+//       <span>身高</span>
+//       <input
+//         ref={cmDOM}
+//         name='cm'
+//         type="text"
+//       /><br/>
+//       <span>體重</span>
+//       <input
+//         ref={kgDOM}
+//         name='kg'
+//         type="text"
+//       /><br/>
+//       <button onClick={handleClick}>計算</button>
+//       BMI : <span data-testid="bmi" ref={bmiDOM} />
+//     </div>
+//   );
+// }
 
 // import React from 'react';
 
@@ -84,3 +84,93 @@ export default function HW4Bmi() {
 //     );
 //   }
 // }
+// class
+// import React, { Component } from 'react';
+
+// export default class HW4Bmi extends Component {
+//   constructor(){
+//     super();
+//     this.state =({
+//       bmi:'',
+//       cm: '',
+//       kg: ''
+//     });
+//   }
+//   handleChange = ({target}) => {
+//     this.setState({
+//       [target.name] : target.value
+//     });
+//   }
+
+//   handleBmi = () => {
+//     const {cm, kg } = this.state;
+//     this.setState({
+//       bmi: ~~(kg / Math.pow((cm / 100), 2)),
+//       cm: '',
+//       kg: ''
+//     });
+//   }
+
+//   render() {
+//     const {bmi, cm , kg } =this.state;
+//     return (
+//       <div>
+//         <h1>複習React & Hooks & Jest</h1>
+//         <form action="">
+//           體 重<input
+//             name='kg'
+//             type="text"
+//             value={kg}
+//             onChange={this.handleChange}
+//           /><br />
+//           身 高<input
+//             name='cm'
+//             type="text"
+//             value={cm}
+//             onChange={this.handleChange}
+//           />
+//         </form>
+//         <button onClick={this.handleBmi}>計算!</button>
+//         <h2>{bmi}</h2>
+//       </div>
+//     );
+//   }
+// }
+
+// hooks
+import React,{useRef} from 'react';
+
+export default function HW4Bmi() {
+  const cmDom = useRef();
+  const kgDom = useRef();
+  const bmiDom = useRef();
+
+  const handleBmi = () => {
+    const kg = kgDom.current.value;
+    const cm = cmDom.current.value;
+    bmiDom.current.innerText = ~~(kg / Math.pow( cm / 100, 2));
+  };
+
+  return (
+    <div>
+      <h1>複習React & Hooks & Jest</h1>
+      <form action="">
+        體 重<input
+          ref={kgDom}
+          name='kg'
+          type="text" 
+        /><br />
+        身 高<input
+          type="text"
+          name='cm'
+          ref={cmDom}
+        />
+      </form>
+      <button onClick={handleBmi}>計算!</button>
+      <h1>BMI : <span data-testid='bmi' ref={bmiDom}/></h1>
+    </div>
+  );
+}
+
+
+
--- src/HW4Bmi.test.js
+++ src/HW4Bmi.test.js
@@ -7,35 +7,48 @@ test('should HW4Bmi', () => {
   expect(container).toMatchSnapshot();
 });
 
-test('should 160cm/60kg get 23', () => {
-  const { container, getByTestId } = render(<HW />);
-  const cmInput = container.querySelector('input[name="cm"]');
-  const kgInput = container.querySelector('input[name="kg"]');
-  const button = container.querySelector('button');
-  const bmiSpan = getByTestId('bmi');
-  
-  userEvent.type(cmInput, '160');
-  userEvent.type(kgInput, '60');
-  userEvent.click(button);
+test('should 90kg / 170cm get 31', () => {
+  const {container , getByTestId } = render(<HW />);
+  const bmi = getByTestId('bmi');
 
-  expect(bmiSpan.innerText).toBe(23);
+  userEvent.type(container.querySelector('input[name="cm"]'), '170');
+  userEvent.type(container.querySelector('input[name="kg"]'), '90');
+  userEvent.click(container.querySelector('button'));
+
+  expect(bmi.innerText).toBe(31);
   expect(container).toMatchSnapshot();
 });
 
-test('should 185cm / 90kg get 26', () => {
-  const { container , getByTestId } = render(<HW/>);
-  const cmInput = container.querySelector('input[name="cm"]');
-  const kgInput = container.querySelector('input[name="kg"]');
-  const button = container.querySelector('button');
-  const bmiSpan = getByTestId('bmi');
-  
-  userEvent.type(cmInput, '185');
-  userEvent.type(kgInput, '90');
-  userEvent.click(button);
 
-  expect(bmiSpan.innerText).toBe(26);
-  expect(container).toMatchSnapshot();
-});
+// test('should 160cm/60kg get 23', () => {
+//   const { container, getByTestId } = render(<HW />);
+//   const cmInput = container.querySelector('input[name="cm"]');
+//   const kgInput = container.querySelector('input[name="kg"]');
+//   const button = container.querySelector('button');
+//   const bmiSpan = getByTestId('bmi');
+  
+//   userEvent.type(cmInput, '160');
+//   userEvent.type(kgInput, '60');
+//   userEvent.click(button);
+
+//   expect(bmiSpan.innerText).toBe(23);
+//   expect(container).toMatchSnapshot();
+// });
+
+// test('should 185cm / 90kg get 26', () => {
+//   const { container , getByTestId } = render(<HW/>);
+//   const cmInput = container.querySelector('input[name="cm"]');
+//   const kgInput = container.querySelector('input[name="kg"]');
+//   const button = container.querySelector('button');
+//   const bmiSpan = getByTestId('bmi');
+  
+//   userEvent.type(cmInput, '185');
+//   userEvent.type(kgInput, '90');
+//   userEvent.click(button);
+
+//   expect(bmiSpan.innerText).toBe(26);
+//   expect(container).toMatchSnapshot();
+// });
 
 
 
