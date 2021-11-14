023d6d31bba1c37609598c7fe88eeaa9b76a96a4
sasie59 <sasie59@gmail.com>
2021-11-14 17:10:20 +0800

    hw1

--- src/HW1Counter.js
+++ src/HW1Counter.js
@@ -1,43 +1,64 @@
-// class
-// import React, { Component } from "react";
-
+// import React, { Component } from 'react';
+// // rcc
 // export default class HW1Counter extends Component {
 //   constructor() {
 //     super();
 //     this.state = {
-//       count: 0,
+//       count: 0
 //     };
 //   }
-//   handleClick = () => [
+
+//   handleClick = () => {
 //     this.setState({
 //       count: this.state.count + 1
-//     })
-//   ]
+//     });
+//   }
 
 //   render() {
 //     const { count } = this.state;
 //     return (
 //       <div>
-//         <h1>複習React & hooks</h1>
-//         <button onClick={this.handleClick}>請按我 : {count}</button>
+//         <h1>複習React</h1>
+//         <button onClick={this.handleClick}>
+//           press me : {count}
+//         </button>
 //       </div>
 //     );
 //   }
 // }
-// hooks
-import React,{useState} from 'react';
+
+// hooks;
+import React, { useState } from 'react';
 
 export default function HW1Counter() {
-  const [count, setCount ] = useState(0);
+
+  const [count, setCount] = useState(0);
 
   const handleClick = () => {
     setCount(count + 1);
   };
+
   return (
     <div>
-      <h1>複習React & Hooks & Jest</h1>
+      <h1>複習Hooks</h1>
       <button onClick={handleClick}>Click Me : {count} </button>
     </div>
   );
 }
+
+// import React,{useState} from 'react';
+
+// export default function HW1Counter() {
+//   const [count, setCount ] = useState(0);
+
+//   const handleClick = () => {
+//     setCount(count + 1);
+//   };
+//   return (
+//     <div>
+//       <h1>複習React & Hooks & Jest</h1>
+//       <button onClick={handleClick}>Click Me : {count} </button>
+//     </div>
+//   );
+// }
 
