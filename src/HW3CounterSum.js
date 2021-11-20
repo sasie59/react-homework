386ded6f2f49bab6014c30913a772c602348c8f4
sasie59 <sasie59@gmail.com>
2021-11-20 11:46:21 +0800

    hw3 hooks 加減乘除

--- src/HW3CounterSum.js
+++ src/HW3CounterSum.js
@@ -16,95 +16,107 @@
 //     </div>
 //   );
 // }
-import React, { Component } from 'react';
 
-export default class HW3CounterSum extends Component {
-  constructor(){
-    super();
-    this.state = ({
-      counter : 1,
-      sum : 0,
-      sum1 : 100,
-      sum2 : 1,
-      sum3 : 10000000000000000000
-    });
-  }
+import React,{useState} from 'react';
 
-  handleAdd = () => {
-    this.setState({
-      counter : this.state.counter + 1,
-      sum : this.state.sum + this.state.counter
-    });
-  }
+export default function HW3CounterSum() {
+  const [counter, setCounter] = useState(1);
+  const [sum, setSum] = useState(0);
+  const [sum1, setSum1] = useState(0);
+  const [sum2, setSum2] = useState(1);
+  const [sum3, setSum3] = useState(100000000000);
 
-handleSubtract = () => {
-  this.setState({
-    counter : this.state.counter + 1,
-    sum1 : this.state.sum1 - this.state.counter
-  });
+  const handleAdd = () => {
+    setCounter(counter + 1);
+    setSum(sum + counter);
+  };
+
+  const handleSubtract = () => {
+    setCounter(counter + 1);
+    setSum1(sum1 - counter);
+  };
+
+  const handleMultiply = () => {
+    setCounter(counter + 1);
+    setSum2(sum2 * counter);
+  };
+
+  const handleDivide = () => {
+    setCounter(counter + 1);
+    setSum3(sum3 / counter);
+  };
+  return (
+    <div>
+      <h1>HW3CounterSum</h1>
+      <button onClick={handleAdd}>add</button><br/>
+      {sum}<br/>
+      <button onClick={handleSubtract}>subtract</button><br/>
+      {sum1}<br/>
+      <button onClick={handleMultiply}>multiply</button><br/>
+      {sum2}<br/>
+      <button onClick={handleDivide}>divide</button><br/>
+      {sum3}<br/>
+    </div>
+  );
 }
 
-  handleMultiply = () => {
-    this.setState({
-      counter : this.state.counter + 1,
-      sum2 : this.state.sum2 * this.state.counter
-    });
-  }
+// import React, { Component } from 'react';
 
-  handleDivide = () => {
-    this.setState({
-      counter : this.state.counter + 1,
-      sum3 : this.state.sum3 / this.state.counter
-    });
-  }
-
-
-
-  render() {
-    const { sum, sum1, sum2, sum3 } = this.state;
-    return (
-      <div>
-        <h1>HW3CounterSum</h1>
-        <button onClick={this.handleAdd}>加</button><br/>
-        {sum}<br />
-        <button onClick={this.handleSubtract}>減</button><br/>
-        {sum1}<br />
-        <button onClick={this.handleMultiply}>乘</button><br/>
-        {sum2}<br />
-        <button onClick={this.handleDivide}>除</button><br/>
-        {sum3}
-      </div>
-    );
-  }
-}
-
-// import React from 'react';
-
-// export default class HW3CounterSum extends React.Component {
+// export default class HW3CounterSum extends Component {
 //   constructor(){
 //     super();
 //     this.state = ({
-//       sum : 0,
 //       counter : 1,
+//       sum : 0,
+//       sum1 : 100,
+//       sum2 : 1,
+//       sum3 : 10000000000000000000
 //     });
 //   }
 
-//   handleClick = () => {
-//     const sum = this.state.sum;
-//     const counter = this.state.counter;
+//   handleAdd = () => {
 //     this.setState({
-//       counter : counter + 1,
-//       sum : sum + counter
-//       // 本打算用梯型工式來算總和 但好像不用那麼麻煩
-
+//       counter : this.state.counter + 1,
+//       sum : this.state.sum + this.state.counter
 //     });
 //   }
+
+// handleSubtract = () => {
+//   this.setState({
+//     counter : this.state.counter + 1,
+//     sum1 : this.state.sum1 - this.state.counter
+//   });
+// }
+
+//   handleMultiply = () => {
+//     this.setState({
+//       counter : this.state.counter + 1,
+//       sum2 : this.state.sum2 * this.state.counter
+//     });
+//   }
+
+//   handleDivide = () => {
+//     this.setState({
+//       counter : this.state.counter + 1,
+//       sum3 : this.state.sum3 / this.state.counter
+//     });
+//   }
+
+
 
 //   render() {
+//     const { sum, sum1, sum2, sum3 } = this.state;
 //     return (
 //       <div>
-//         <button onClick={this.handleClick}><b>add it</b></button><br />
-//         {this.state.sum}
+//         c
+//         <button onClick={this.handleAdd}>加</button><br/>
+//         {sum}<br />
+//         <button onClick={this.handleSubtract}>減</button><br/>
+//         {sum1}<br />
+//         <button onClick={this.handleMultiply}>乘</button><br/>
+//         {sum2}<br />
+//         <button onClick={this.handleDivide}>除</button><br/>
+//         {sum3}
 //       </div>
 //     );
 //   }
