6b867fdd015c24beaaf12a1bab946faefd85503c
sasie59 <sasie59@gmail.com>
2021-11-20 22:47:29 +0800

    hw4 rfc bmi

--- src/HW4Bmi.js
+++ src/HW4Bmi.js
@@ -1,176 +1,92 @@
-// import React ,{ useRef }from 'react';
+// class
+import React, { Component } from 'react';
+
+export default class HW4Bmi extends Component {
+  constructor(){
+    super();
+    this.state = ({
+      cm: '',
+      kg: '',
+      bmi: ''
+    });
+  }
+
+  handleChange = ({target}) => {
+    this.setState({
+      [target.name] : target.value
+    });
+  }
+
+  handleBmi = () => {
+    const { kg, cm } = this.state;
+    this.setState({
+      // 體重(公斤) / 身高2(公尺2). 
+      bmi: Math.floor(kg / Math.pow((cm / 100), 2)),
+      cm: '',
+      kg: ''
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>HW4Bmi</h1>
+        <form action="">
+          體重<input
+            name="kg"
+            type="text"
+            value={this.state.kg}
+            onChange={this.handleChange}
+          /><br/>
+          身高<input
+            name="cm"
+            type="text"
+            value={this.state.cm}
+            onChange={this.handleChange}    
+          /><br/>
+        </form>
+        <button onClick={this.handleBmi}>計算</button>
+        <h2>BMI : {this.state.bmi}</h2>
+      </div>
+    );
+  }
+}
+
+
+// hooks
+// import React,{useRef} from 'react';
 
 // export default function HW4Bmi() {
-//   const cmDOM = useRef();
-//   const kgDOM = useRef();
-//   const bmiDOM = useRef();
+//   const cmDom = useRef();
+//   const kgDom = useRef();
+//   const bmiDom = useRef();
 
-//   const handleClick = () => {
-//     const cm = cmDOM.current.value;
-//     const kg = kgDOM.current.value;
-//     bmiDOM.current.innerText = Math.floor(kg / Math.pow(cm / 100, 2));
+//   const handleBmi = () => {
+//     const kg = kgDom.current.value;
+//     const cm = cmDom.current.value;
+//     bmiDom.current.innerText = ~~(kg / Math.pow( cm / 100, 2));
 //   };
 
 //   return (
 //     <div>
-//       <h1>HW4Bmi</h1>
-//       <span>身高</span>
-//       <input
-//         ref={cmDOM}
-//         name='cm'
-//         type="text"
-//       /><br/>
-//       <span>體重</span>
-//       <input
-//         ref={kgDOM}
-//         name='kg'
-//         type="text"
-//       /><br/>
-//       <button onClick={handleClick}>計算</button>
-//       BMI : <span data-testid="bmi" ref={bmiDOM} />
+//       <h1>複習React & Hooks & Jest</h1>
+//       <form action="">
+//         體 重<input
+//           ref={kgDom}
+//           name='kg'
+//           type="text" 
+//         /><br />
+//         身 高<input
+//           type="text"
+//           name='cm'
+//           ref={cmDom}
+//         />
+//       </form>
+//       <button onClick={handleBmi}>計算!</button>
+//       <h1>BMI : <span data-testid='bmi' ref={bmiDom}/></h1>
 //     </div>
 //   );
 // }
-
-// import React from 'react';
-
-// export default class HW4Bmi extends React.Component {
-//   constructor(){
-//     super();
-//     this.state = ({
-//       cm : '',
-//       kg : '',
-//       BMI : ''
-//     });
-//   }
-//   handleChange = ({ target }) => {
-//     this.setState({
-//       [target.name]: target.value
-//     });
-//   }
-
-//   calculateBMI = () => {
-//     const { kg, cm }  = this.state;
-    
-//     this.setState({
-//       BMI : Math.floor(kg / Math.pow(cm / 100, 2))
-//     });
-//   }
-//   render() {
-
-//     return (
-//       <div>
-//         <form>
-//         身高:
-//         <input
-//           id=""
-//           name="cm"
-//           type="text"
-//           value={this.state.cm}
-//           onChange={this.handleChange}
-//         />cm<br />
-//         體重:
-//         <input
-//           id=""
-//           name="kg"
-//           type="text"
-//           value={this.state.kg}
-//           onChange={this.handleChange}
-//         />kg<br />
-//         </form>
-//         <button onClick={this.calculateBMI}>計算!</button>
-//         <h2>BMI : {this.state.BMI}</h2>
-//       </div>
-//     );
-//   }
-// }
-// class
-// import React, { Component } from 'react';
-
-// export default class HW4Bmi extends Component {
-//   constructor(){
-//     super();
-//     this.state =({
-//       bmi:'',
-//       cm: '',
-//       kg: ''
-//     });
-//   }
-//   handleChange = ({target}) => {
-//     this.setState({
-//       [target.name] : target.value
-//     });
-//   }
-
-//   handleBmi = () => {
-//     const {cm, kg } = this.state;
-//     this.setState({
-//       bmi: ~~(kg / Math.pow((cm / 100), 2)),
-//       cm: '',
-//       kg: ''
-//     });
-//   }
-
-//   render() {
-//     const {bmi, cm , kg } =this.state;
-//     return (
-//       <div>
-//         <h1>複習React & Hooks & Jest</h1>
-//         <form action="">
-//           體 重<input
-//             name='kg'
-//             type="text"
-//             value={kg}
-//             onChange={this.handleChange}
-//           /><br />
-//           身 高<input
-//             name='cm'
-//             type="text"
-//             value={cm}
-//             onChange={this.handleChange}
-//           />
-//         </form>
-//         <button onClick={this.handleBmi}>計算!</button>
-//         <h2>{bmi}</h2>
-//       </div>
-//     );
-//   }
-// }
-
-// hooks
-import React,{useRef} from 'react';
-
-export default function HW4Bmi() {
-  const cmDom = useRef();
-  const kgDom = useRef();
-  const bmiDom = useRef();
-
-  const handleBmi = () => {
-    const kg = kgDom.current.value;
-    const cm = cmDom.current.value;
-    bmiDom.current.innerText = ~~(kg / Math.pow( cm / 100, 2));
-  };
-
-  return (
-    <div>
-      <h1>複習React & Hooks & Jest</h1>
-      <form action="">
-        體 重<input
-          ref={kgDom}
-          name='kg'
-          type="text" 
-        /><br />
-        身 高<input
-          type="text"
-          name='cm'
-          ref={cmDom}
-        />
-      </form>
-      <button onClick={handleBmi}>計算!</button>
-      <h1>BMI : <span data-testid='bmi' ref={bmiDom}/></h1>
-    </div>
-  );
-}
 
 
 
