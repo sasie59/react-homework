71a820f350d7e25acab18a266eed124d98c9b737
sasie59 <sasie59@gmail.com>
2021-11-20 11:31:43 +0800

    HW3 rcc 加減乘除

--- src/HW3CounterSum.js
+++ src/HW3CounterSum.js
@@ -1,20 +1,81 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 
-export default function HW3CounterSum() {
-  const [count, setCount] = useState(0);
-  const [sum, setSum] = useState(0);
-  return (
-    <div>
-      <h1>HW3CounterSum</h1>
-      <button
-        onClick={() => {
-          setCount(count + 1);
-          setSum(sum + count);
-        }}>add
-      </button><br />
-      {sum}
-    </div>
-  );
+// export default function HW3CounterSum() {
+//   const [count, setCount] = useState(0);
+//   const [sum, setSum] = useState(0);
+//   return (
+//     <div>
+//       <h1>HW3CounterSum</h1>
+//       <button
+//         onClick={() => {
+//           setCount(count + 1);
+//           setSum(sum + count);
+//         }}>add
+//       </button><br />
+//       {sum}
+//     </div>
+//   );
+// }
+import React, { Component } from 'react';
+
+export default class HW3CounterSum extends Component {
+  constructor(){
+    super();
+    this.state = ({
+      counter : 1,
+      sum : 0,
+      sum1 : 100,
+      sum2 : 1,
+      sum3 : 10000000000000000000
+    });
+  }
+
+  handleAdd = () => {
+    this.setState({
+      counter : this.state.counter + 1,
+      sum : this.state.sum + this.state.counter
+    });
+  }
+
+handleSubtract = () => {
+  this.setState({
+    counter : this.state.counter + 1,
+    sum1 : this.state.sum1 - this.state.counter
+  });
+}
+
+  handleMultiply = () => {
+    this.setState({
+      counter : this.state.counter + 1,
+      sum2 : this.state.sum2 * this.state.counter
+    });
+  }
+
+  handleDivide = () => {
+    this.setState({
+      counter : this.state.counter + 1,
+      sum3 : this.state.sum3 / this.state.counter
+    });
+  }
+
+
+
+  render() {
+    const { sum, sum1, sum2, sum3 } = this.state;
+    return (
+      <div>
+        <h1>HW3CounterSum</h1>
+        <button onClick={this.handleAdd}>加</button><br/>
+        {sum}<br />
+        <button onClick={this.handleSubtract}>減</button><br/>
+        {sum1}<br />
+        <button onClick={this.handleMultiply}>乘</button><br/>
+        {sum2}<br />
+        <button onClick={this.handleDivide}>除</button><br/>
+        {sum3}
+      </div>
+    );
+  }
 }
 
 // import React from 'react';
