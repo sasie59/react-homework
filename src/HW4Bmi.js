985a19ed2c2ed97b1b17a3c250efe5c42d7840c5
sasie59 <sasie59@gmail.com>
2021-11-21 21:48:01 +0800

    hw4 bmi hooks

--- src/HW4Bmi.js
+++ src/HW4Bmi.js
@@ -1,92 +1,89 @@
 // class
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 
-export default class HW4Bmi extends Component {
-  constructor(){
-    super();
-    this.state = ({
-      cm: '',
-      kg: '',
-      bmi: ''
-    });
-  }
+// export default class HW4Bmi extends Component {
+//   constructor(){
+//     super();
+//     this.state = ({
+//       cm: '',
+//       kg: '',
+//       bmi: ''
+//     });
+//   }
 
-  handleChange = ({target}) => {
-    this.setState({
-      [target.name] : target.value
-    });
-  }
+//   handleChange = ({target}) => {
+//     this.setState({
+//       [target.name] : target.value
+//     });
+//   }
 
-  handleBmi = () => {
-    const { kg, cm } = this.state;
-    this.setState({
-      // 體重(公斤) / 身高2(公尺2). 
-      bmi: Math.floor(kg / Math.pow((cm / 100), 2)),
-      cm: '',
-      kg: ''
-    });
-  }
+//   handleBmi = () => {
+//     const { kg, cm } = this.state;
+//     this.setState({
+//       // 體重(公斤) / 身高2(公尺2). 
+//       bmi: Math.floor(kg / Math.pow((cm / 100), 2)),
+//       cm: '',
+//       kg: ''
+//     });
+//   }
 
-  render() {
-    return (
-      <div>
-        <h1>HW4Bmi</h1>
-        <form action="">
-          體重<input
-            name="kg"
-            type="text"
-            value={this.state.kg}
-            onChange={this.handleChange}
-          /><br/>
-          身高<input
-            name="cm"
-            type="text"
-            value={this.state.cm}
-            onChange={this.handleChange}    
-          /><br/>
-        </form>
-        <button onClick={this.handleBmi}>計算</button>
-        <h2>BMI : {this.state.bmi}</h2>
-      </div>
-    );
-  }
-}
-
-
-// hooks
-// import React,{useRef} from 'react';
-
-// export default function HW4Bmi() {
-//   const cmDom = useRef();
-//   const kgDom = useRef();
-//   const bmiDom = useRef();
-
-//   const handleBmi = () => {
-//     const kg = kgDom.current.value;
-//     const cm = cmDom.current.value;
-//     bmiDom.current.innerText = ~~(kg / Math.pow( cm / 100, 2));
-//   };
-
-//   return (
-//     <div>
-//       <h1>複習React & Hooks & Jest</h1>
-//       <form action="">
-//         體 重<input
-//           ref={kgDom}
-//           name='kg'
-//           type="text" 
-//         /><br />
-//         身 高<input
-//           type="text"
-//           name='cm'
-//           ref={cmDom}
-//         />
-//       </form>
-//       <button onClick={handleBmi}>計算!</button>
-//       <h1>BMI : <span data-testid='bmi' ref={bmiDom}/></h1>
-//     </div>
-//   );
+//   render() {
+//     return (
+//       <div>
+//         <h1>HW4Bmi</h1>
+//         <form action="">
+//           體重<input
+//             name="kg"
+//             type="text"
+//             value={this.state.kg}
+//             onChange={this.handleChange}
+//           /><br/>
+//           身高<input
+//             name="cm"
+//             type="text"
+//             value={this.state.cm}
+//             onChange={this.handleChange}    
+//           /><br/>
+//         </form>
+//         <button onClick={this.handleBmi}>計算</button>
+//         <h2>BMI : {this.state.bmi}</h2>
+//       </div>
+//     );
+//   }
 // }
+// hooks
+import React,{useRef} from 'react';
 
+export default function HW4Bmi() {
+  const kgDom = useRef();
+  const cmDom = useRef();
+  const bmiDom = useRef();
 
+  const handleClick = () => {
+    const kg = kgDom.current.value;
+    const cm = cmDom.current.value;
+    bmiDom.current.innerText = ~~(kg / Math.pow((cm / 100), 2));
+  };
+
+  return (
+    <div>
+      <h1>HW4Bmi</h1>
+      <form action="">
+        體重<input 
+          ref={kgDom}
+          type="text"
+          name='kg'/><br/>
+        身高<input
+          ref={cmDom}
+          type="text"
+          name='cm'/>                
+      </form>
+      <button onClick={handleClick}>點擊</button>
+      <h2>
+        BMI :
+        <span ref={bmiDom} />
+      </h2>
+    </div>
+  );
+}
 
