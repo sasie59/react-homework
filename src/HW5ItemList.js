c9350df01285397ab3c15ad4e6d17e427521694f
sasie59 <sasie59@gmail.com>
2021-11-23 23:23:33 +0800

    hw5 hooks

--- src/HW5ItemList.js
+++ src/HW5ItemList.js
@@ -29,53 +29,86 @@
 //     </div>
 //   );
 // }
-import React, { Component } from 'react';
+import React,{useState, useRef} from 'react';
 
-export default class HW5ItemList extends Component {
-  constructor() {
-    super();
-    this.state = ({
-      value: '',
-      list: []
-    });
-  }
+export default function HW5ItemList() {
+  const [list, setList] = useState([]);
+  const inputDom = useRef();
 
-  handleChange = (e) => {
-    this.setState({
-      value: e.target.value
-    });
-  }
-
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    this.setState({
-      list: [this.state.value, ...this.state.list],
-      value: ''
-    });
-  }
+    setList([inputDom.current.value, ...list]);
+    inputDom.current.value = '';
 
-  render() {
-    return (
-      <div>
-        <h1>HW5ItemList</h1>
-        <form
-          action=""
-          onSubmit={this.handleSubmit}>
-          <input
-            type="text"
-            value={this.state.value}
-            onChange={this.handleChange}
-            placeholder="do something..."
-          />
-        </form>
-        <ul>
-          {this.state.list.map(item =>
-            <li key={item}>
-              {item}
-            </li>
-          )}
-        </ul>
-      </div>
-    );
-  }
+  };
+
+  return (
+    <div>
+      <h1>HW5ItemList</h1>
+      <form onSubmit={handleSubmit} action="">
+        <input
+          type="text"
+          ref={inputDom}
+        />
+      </form>
+      <ul>
+        {list.map(item => 
+          <li key={item}>
+            {item}
+          </li>
+        )}
+      </ul>
+    </div>
+  );
 }
+
+// import React, { Component } from 'react';
+
+// export default class HW5ItemList extends Component {
+//   constructor() {
+//     super();
+//     this.state = ({
+//       value: '',
+//       list: []
+//     });
+//   }
+
+//   handleChange = (e) => {
+//     this.setState({
+//       value: e.target.value
+//     });
+//   }
+
+//   handleSubmit = (e) => {
+//     e.preventDefault();
+//     this.setState({
+//       list: [this.state.value, ...this.state.list],
+//       value: ''
+//     });
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <h1>HW5ItemList</h1>
+//         <form
+//           action=""
+//           onSubmit={this.handleSubmit}>
+//           <input
+//             type="text"
+//             value={this.state.value}
+//             onChange={this.handleChange}
+//             placeholder="do something..."
+//           />
+//         </form>
+//         <ul>
+//           {this.state.list.map(item =>
+//             <li key={item}>
+//               {item}
+//             </li>
+//           )}
+//         </ul>
+//       </div>
+//     );
+//   }
+// }
