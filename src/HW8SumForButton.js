fdef909d07ad03af7465a9b12866e3658cd82d29
sasie59 <sasie59@gmail.com>
2021-11-29 23:27:41 +0800

    hw8 rcc

--- src/HW8SumForButton.js
+++ src/HW8SumForButton.js
@@ -1,21 +1,52 @@
-import React,{ useState } from 'react';
+// import React,{ useState } from 'react';
 
-export default function HW8SumForButton() {
+// export default function HW8SumForButton() {
 
-  const [ sum, setSum ] = useState(0);
+//   const [ sum, setSum ] = useState(0);
 
-  return (
-    <div>
-      <h1>HW8SumForButton</h1>
-      {[1,2,3,4,5,6,7,8,9].map((value,index) => 
-        <button
-          data-testid={index}
-          key={index}
-          onClick={() => setSum(sum + value)}
-        >{value}</button>
-      )}
-      <div name='sum'>加總值 : {sum}</div>
-    </div>
-  );
+//   return (
+//     <div>
+//       <h1>HW8SumForButton</h1>
+//       {[1,2,3,4,5,6,7,8,9].map((value,index) => 
+//         <button
+//           data-testid={index}
+//           key={index}
+//           onClick={() => setSum(sum + value)}
+//         >{value}</button>
+//       )}
+//       <div name='sum'>加總值 : {sum}</div>
+//     </div>
+//   );
+// }
+
+import React, { Component } from 'react';
+
+export default class HW8SumForButton extends Component {
+  constructor(){
+    super();
+    this.state = ({
+      sum : 0
+    });
+  }
+
+  handleClick = (value) => {
+    this.setState({
+      sum : this.state.sum + value
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h1>HW8SumForButton</h1>
+        {[1,2,3,4,5,6,7,8,9].map(item => 
+          <button onClick={this.handleClick.bind(this,item)}>
+            {item}
+          </button>
+        )}
+        <h2>加總值 : {this.state.sum}</h2>
+      </div>
+    );
+  }
 }
+
 
