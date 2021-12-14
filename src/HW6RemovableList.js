ab0f029f9af7c14c09dcc032f882988be5905147
sasie59 <sasie59@gmail.com>
2021-12-14 23:46:46 +0800

    hw6  rcc (bind的用法)

--- src/HW6RemovableList.js
+++ src/HW6RemovableList.js
@@ -1,42 +1,103 @@
-import React, { useState,useRef } from "react";
+// import React, { useState, useRef } from "react";
 
-export default function HW6RemovableList() {
-  const [list, setList] = useState([]);
-  const itemDom = useRef();
+// export default function HW6RemovableList() {
+//   const [list, setList] = useState([]);
+//   const itemDom = useRef();
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    setList([itemDom.current.value, ...list]);
-    itemDom.current.value = '';
-  };
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+//     setList([itemDom.current.value, ...list]);
+//     itemDom.current.value = '';
+//   };
 
 
-  return (
-    <div>
-      <h1>HW6RemovableList</h1>
-      <form
-        onSubmit={handleSubmit}
-      >
-        <input
-          type="text"
-          ref={itemDom}
-        />
-      </form>
-      <ul>
-        {list.map((item,index) => (
-          <li key={index}>
-            {item}
-            <button
-              data-testid={index}
-              onClick={()=> setList.bind(index)(
-                [...list.slice(0, index),...list.slice(index + 1)]
-              )}>Clear it!
-            </button>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+//   return (
+//     <div>
+//       <h1>HW6RemovableList</h1>
+//       <form
+//         onSubmit={handleSubmit}
+//       >
+//         <input
+//           type="text"
+//           ref={itemDom}
+//         />
+//       </form>
+//       <ul>
+//         {list.map((item, index) => (
+//           <li key={index}>
+//             {item}
+//             <button
+//               data-testid={index}
+//               onClick={() => setList.bind(index)(
+//                 [...list.slice(0, index), ...list.slice(index + 1)]
+//               )}>Clear it!
+//             </button>
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+import React, { Component } from 'react';
+
+export default class HW6RemovableList extends Component {
+  constructor() {
+    super();
+    this.state = ({
+      list: [],
+      value: ''
+    });
+  }
+
+  handleChange = ({ target }) => {
+    this.setState({
+      value: target.value
+    });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    if (this.state.value !== '')
+      this.setState({
+        list: [this.state.value, ...this.state.list],
+        value: ''
+      });
+  }
+
+  handleRemove = (index) => {
+    const filterList = this.state.list.filter((item, index1) =>
+      index !== index1
+    );
+    this.setState({
+      list: filterList
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>HW6RemovableList</h1>
+        <form
+          action=""
+          onSubmit={this.handleSubmit}
+        >
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+        </form>
+        <ul>
+          {this.state.list.map((item, index) =>
+            <li key={item}>
+              {item}
+              <button onClick={this.handleRemove.bind(this, index)}>移除</button>
+            </li>
+          )}
+        </ul>
+      </div>
+    );
+  }
 }
 
 // import React from 'react';
