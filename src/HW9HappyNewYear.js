9c039ab38797baaad2f69848e5e5eee31133fe99
sasie59 <sasie59@gmail.com>
2021-12-23 16:09:03 +0800

    hw9 跨年 hooks

--- src/HW9HappyNewYear.js
+++ src/HW9HappyNewYear.js
@@ -37,38 +37,72 @@
 //     </div>
 //   );
 // }
-
-import React, { Component } from 'react';
+import React,{useState, useEffect} from 'react';
 const date20220101 = +new Date('2022/01/01 00:00:00');
+export default function HW9HappyNewYear() {
 
-export default class HW9HappyNewYear extends Component {
-  constructor(){
-    super();
-    this.state = ({
-      countDown: 0,
-      date: +new Date()
+  const [time, setTime] = useState({
+    date : +new Date(),
+    conutDown : ~~(date20220101 - (+new Date()) / 1000),
+  });
+
+  const tick = () => {
+    setTime({
+      countDown : Math.floor((date20220101 - (+new Date())) / 1000),
     });
-  }
-  tick = () => {
-    this.setState({
-      date: +new Date(),
-      countDown: ~~((date20220101 - this.state.date) / 1000)
-    });
-  }
-  componentDidMount() {
-    setInterval(this.tick, 1000);
-  }
+  };
+
+  useEffect(() => {
+    setInterval(tick(), 1000);
+  },[]);
+
+  const sec =  time.countDown % 60;
+  const min =  ~~(time.countDown / 60) % 60;
+  const hour = ~~(time.countDown / 60 / 60) % 24;
+  const day =  ~~(time.countDown / 60 / 60 / 24);
+
   
-  render() {
-    const{countDown} = this.state;
-    const sec =  countDown % 60;
-    const min =  ~~(countDown / 60) % 60;
-    const hour = ~~(countDown / 60 / 60) % 24;
-    const day =  ~~(countDown / 60 / 60 / 24);
-    return (
-      <div>
-        <h1 data-testid='countDown'>離2022跨年還有{day}天{hour}時{min}分{sec}秒: </h1>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h1>HW9HappyNewYear</h1>
+      <h1>
+      距離2022跨年還有 : {day}天 {hour}時 {min}分 {sec}秒
+      </h1>
+    </div>
+  );
 }
+
+// import React, { Component } from 'react';
+// const date20220101 = +new Date('2022/01/01 00:00:00');
+
+// export default class HW9HappyNewYear extends Component {
+//   constructor(){
+//     super();
+//     this.state = ({
+//       countDown: 0,
+//       date: +new Date()
+//     });
+//   }
+//   tick = () => {
+//     this.setState({
+//       date: +new Date(),
+//       countDown: ~~((date20220101 - this.state.date) / 1000)
+//     });
+//   }
+//   componentDidMount() {
+//     setInterval(this.tick, 1000);
+//   }
+  
+//   render() {
+//     const{countDown} = this.state;
+//     const sec =  countDown % 60;
+//     const min =  ~~(countDown / 60) % 60;
+//     const hour = ~~(countDown / 60 / 60) % 24;
+//     const day =  ~~(countDown / 60 / 60 / 24);
+//     return (
+//       <div>
+//         <h1 data-testid='countDown'>離2022跨年還有{day}天{hour}時{min}分{sec}秒: </h1>
+//       </div>
+//     );
+//   }
+// }
