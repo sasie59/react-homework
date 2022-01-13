e73a516a4edc9cbf227f3d653ec5d81c479862d7
sasie59 <sasie59@gmail.com>
2022-01-13 23:39:30 +0800

    hw11 rcc 調色盤

--- src/HW11ColorPad.js
+++ src/HW11ColorPad.js
@@ -1,68 +1,124 @@
-import React, { useState } from "react";
+import React, { Component } from 'react';
 
-export default function HW11ColorPad() {
-  const [pad, setPad] = useState({
-    r: 0,
-    g: 150,
-    b: 150,
-  });
+export default class HW11ColorPad extends Component {
+  constructor() {
+    super();
+    this.state = {
+      r: 150,
+      g: 100,
+      b: 200
+    };
+  }
 
-  const handleChange = (color, {target}) => {
-    setPad({
-      ...pad,
-      [color]: target.value
+  handleChange = ({ target }) => {
+    this.setState({
+      [target.name]: target.value
     });
-  };
-  // 展開後 再去改變他們各自的值
+  }
 
-  return (
-    <div>
-      <h1>HW11ColorPad</h1>
-      <h1>調色盤</h1>
-      <div
-        style={{
+  render() {
+    const { r, g, b } = this.state;
+    return (
+      <div>
+        <h1>HW11ColorPad</h1>
+        <h1>調色盤</h1>
+        <div style={{
           width: "200px",
           height: "200px",
           margin: "0 auto",
-          backgroundColor: `rgb(${pad.r},${pad.g},${pad.b})`,
-        }}
-      />
-      R
-      <input
-        type="range"
-        min="0"
-        max="255"
-        data-testid='red'
-        onChange={handleChange.bind(this, 'r')}
-      />{pad.r}
-      <br />
-      G
-      <input
-        type="range"
-        min="0"
-        max="255"
-        data-testid='green'
-        onChange={handleChange.bind(this, 'g')}
-      />{pad.g}
-      <br />
-      B
-      <input
-        type="range"
-        min="0"
-        max="255"
-        data-testid='blue'
-        onChange={handleChange.bind(this, 'b')}
-      />{pad.b}
-      <br />
-    </div>
-  );
+          backgroundColor: `rgb(${r},${g},${b})`
+        }} />
+        <input
+          type="range"
+          min="0"
+          max="255"
+          name='r'
+          value={r}
+          data-testid='red'
+          onChange={this.handleChange}
+        />R<br />
+        <input
+          type="range"
+          min="0"
+          max="255"
+          name='g'
+          value={g}
+          data-testid='green'
+          onChange={this.handleChange}
+        />G<br />
+        <input
+          type="range"
+          min="0"
+          max="255"
+          name='b'
+          value={b}
+          data-testid='blue'
+          onChange={this.handleChange}
+        />B
+      </div>
+    );
+  }
 }
-// 錯誤警告
-// Warning: A component is changing a controlled input
-//  of type range to be uncontrolled. Input elements should not
-//   switch from controlled to uncontrolled (or vice versa). 
-//   Decide between using a controlled or uncontrolled 
-//   input element for the lifetime of the component.
+
+// import React, { useState } from "react";
+
+// export default function HW11ColorPad() {
+//   const [pad, setPad] = useState({
+//     r: 0,
+//     g: 150,
+//     b: 150,
+//   });
+
+//   const handleChange = (color, { target }) => {
+//     setPad({
+//       ...pad,
+//       [color]: target.value
+//     });
+//   };
+//   // 展開後 再去改變他們各自的值
+
+//   return (
+//     <div>
+//       <h1>HW11ColorPad</h1>
+//       <h1>調色盤</h1>
+//       <div
+//         style={{
+//           width: "200px",
+//           height: "200px",
+//           margin: "0 auto",
+//           backgroundColor: `rgb(${pad.r},${pad.g},${pad.b})`,
+//         }}
+//       />
+//       R
+//       <input
+//         type="range"
+//         min="0"
+//         max="255"
+//         data-testid='red'
+//         onChange={handleChange.bind(this, 'r')}
+//       />{pad.r}
+//       <br />
+//       G
+//       <input
+//         type="range"
+//         min="0"
+//         max="255"
+//         data-testid='green'
+//         onChange={handleChange.bind(this, 'g')}
+//       />{pad.g}
+//       <br />
+//       B
+//       <input
+//         type="range"
+//         min="0"
+//         max="255"
+//         data-testid='blue'
+//         onChange={handleChange.bind(this, 'b')}
+//       />{pad.b}
+//       <br />
+//     </div>
+//   );
+// }
 
 // import React, { Component } from 'react';
 
