7fa233e0b7beeadba847b092ff00fcdda1de50f7
sasie59 <sasie59@gmail.com>
2022-01-24 22:58:27 +0800

    hw12 rcc 標籤

--- src/HW12Tab.js
+++ src/HW12Tab.js
@@ -1,34 +1,79 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 
+// const arr = ["lorem 1", "lorem 2", "lorem 3", "lorem 4", "lorem 5"];
+
+// export default function HW12Tab() {
+//   const [index, setIndex] = useState(0);
+
+//   const handleClick = (index) => {
+//     setIndex(index);
+//   };
+
+//   return (
+//     <div>
+//       <h1>HW12Tab</h1>
+//       <h1>標籤</h1>
+//       {["tab1", "tab2", "tab3", "tab4", "tab5"].map((item, index) => (
+//         <div
+//           key={index}
+//           data-testid={index}
+//           onClick={handleClick.bind(this, index)}
+//           style={{
+//             display: "inline-block",
+//             width: "50px",
+//             height: "25px",
+//             border: "1px solid black",
+//           }}>{item}
+//         </div>
+//       ))}<br />
+//       {arr[index]}
+//     </div>
+//   );
+// }
+import React, { Component } from 'react';
 const arr = ["lorem 1", "lorem 2", "lorem 3", "lorem 4", "lorem 5"];
+export default class HW12Tab extends Component {
+  constructor() {
+    super();
+    this.state = {
+      index: 0
+    };
+  }
 
-export default function HW12Tab() {
-  const [index, setIndex] = useState(0);
+  handleClick = (index) => {
+    this.setState({
+      index
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h1>HW12Tab</h1>
+        <h1>標籤</h1>
+        <h2>請試著點擊</h2>
+        {["tab1", "tab2", "tab3", "tab4", "tab5"].map((item, index) =>
+          <div
+            data-testid={index}
+            key={index}
+            style={{
+              display: 'inline-block',
+              width: '50px',
+              height: '25px',
+              border: '1px solid black',
+              borderRadius: '10px',
+              margin: '2px',
+              backgroundColor: 'rgb(100, 100, 255)'
+            }}
+            onClick={this.handleClick.bind(this, index)}
+          >
+            {item}
+          </div>
+        )}<br />
+        {arr[this.state.index]}
+      </div>
 
-  const handleClick = (index) => {
-    setIndex(index);
-  };
-
-  return (
-    <div>
-      <h1>HW12Tab</h1>
-      <h1>標籤</h1>
-      {["tab1", "tab2", "tab3", "tab4", "tab5"].map((item, index) => (
-        <div
-          key={index}
-          data-testid={index}
-          onClick={handleClick.bind(this, index)}
-          style={{
-            display: "inline-block",
-            width: "50px",
-            height: "25px",
-            border: "1px solid black",
-          }}>{item}
-        </div>
-      ))}<br />
-      {arr[index]}
-    </div>
-  );
+    );
+  }
 }
 
 /* // import React, { Component } from 'react'; */
